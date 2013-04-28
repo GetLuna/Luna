11608f6483c411b8f978b26886c57656a8b4162b
Yannick <contact@studio384.be>
2013-04-28 20:56:04 +0200

    #8 Spelling/grammar errors in comments

--- edit.php
+++ edit.php
@@ -49,7 +49,7 @@ if ($is_admmod && $pun_user['g_id'] != PUN_ADMIN && in_array($cur_post['poster_i
 	message($lang_common['No permission'], false, '403 Forbidden');  
 
 
-// Load the post.php language file
+// Load the post.php language files
 require PUN_ROOT.'lang/'.$pun_user['language'].'/post.php';
 
 // Start with a clean slate
