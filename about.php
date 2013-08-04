d93e9a958ccc3b6fcc39f71272d998b96c2c1191
Yannick <contact@studio384.be>
2013-08-04 15:44:21 +0200

    Update about page

--- about.php
+++ about.php
@@ -47,13 +47,15 @@ if (preg_match("/^[0-9.-]{1,}$/", $latest_version)) {
 	<a href="http://modernbb.be/downloads/<?php echo FORUM_VERSION ?>.zip" class="btn btn-primary">Download v<?php echo FORUM_VERSION ?></a>
 </div>
 <div class="content">
-    <h2>What's new in version 2.0-beta.1?</h2>
-    <h3>Brand new Dashboard</h3>
+    <h2>What's new in version 2.0-beta.2?</h2>
+    <h3>The new dashboard: Backstage</h3>
     <img src="admin/img/dashboard.png" width="1065" height="250" alt="The new dashboard design" />
 	<div class="row-fluid">
-      <div class="span6"><p><b>Modern design.</b><br />The dashboard has a brand new Bootstrap-based design, making ModernBB easier to use.</p></div>
+      <div class="span6"><p><b>Modern design.</b><br />
+      The dashboard has a brand new design, we call it Aurora, this Bootstrap based design is made to be modern. Backstage is made to make ModernBB easy to use.</p></div>
       <div class="span6"><p><b>Make it your own.</b><br />The new dashboard gives you the posebility to costumize it as much as you want with Bootstap themes.</p></div>
-      <p><b>Modern standards. No dust. More features.</b><br />The revamped dashboard is HTML5 and CSS3 based, instead of XHTML1.1, and doesn't affect the front-end of your forums anymore. We improved lots of features, like creating new forums. But we also added new features, it's now possible to create back-ups out-of-the-box. We use more placeholders and we say "goodbye" to not-functional HTML.</p>
+      <p><b>Modern standards. No dust. More features.</b><br />
+      Backstage is HTML5 and CSS3 based, instead of XHTML1.1, and doesn't affect the front-end of your forums anymore. We improved lots of features, like creating new forums. But we also added new features, it's now possible to create back-ups out-of-the-box. We use more placeholders and we say "goodbye" to not-functional HTML.</p>
 	</div>
 	<h3>Checks for updates, always</h3>
     <img src="admin/img/update.png" width="981" height="89" alt="The new update" />
