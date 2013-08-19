d090af51330235936f56b95cef1abc0c1a473473
Yannick <contact@studio384.be>
2013-08-19 14:31:32 +0200

    Fix installer

--- install.php
+++ install.php
@@ -146,7 +146,7 @@ if (!isset($_POST['form_sent']))
 	$title = $lang_install['My ModernBB Forum'];
 	$description = '<p><span>'.$lang_install['Description'].'</span></p>';
 	$default_lang = $install_lang;
-	$default_style = 'Air';
+	$default_style = 'Randomness';
 }
 else
 {
