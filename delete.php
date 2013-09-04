561372415ed2c542575e0c5274430aab78a41c28
Yannick <contact@studio384.be>
2013-09-04 20:40:02 +0200

    #163 Update delete with new interface

--- delete.php
+++ delete.php
@@ -86,53 +86,32 @@ require FORUM_ROOT.'include/parser.php';
 $cur_post['message'] = parse_message($cur_post['message'], $cur_post['hide_smilies']);
 
 ?>
-<div class="linkst">
-	<div class="inbox">
-		<ul class="crumbs">
-			<li><a href="index.php"><?php echo $lang_common['Index'] ?></a></li>
-			<li><span>»&#160;</span><a href="viewforum.php?id=<?php echo $cur_post['fid'] ?>"><?php echo pun_htmlspecialchars($cur_post['forum_name']) ?></a></li>
-			<li><span>»&#160;</span><a href="viewtopic.php?pid=<?php echo $id ?>#p<?php echo $id ?>"><?php echo pun_htmlspecialchars($cur_post['subject']) ?></a></li>
-			<li><span>»&#160;</span><strong><?php echo $lang_front['Delete post'] ?></strong></li>
-		</ul>
-	</div>
-</div>
+<ul class="breadcrumb">
+    <li><a href="index.php"><?php echo $lang_common['Index'] ?></a></li>
+    <li><a href="viewforum.php?id=<?php echo $cur_post['fid'] ?>"><?php echo pun_htmlspecialchars($cur_post['forum_name']) ?></a></li>
+    <li><a href="viewtopic.php?pid=<?php echo $id ?>#p<?php echo $id ?>"><?php echo pun_htmlspecialchars($cur_post['subject']) ?></a></li>
+    <li class="active"><?php echo $lang_front['Delete post'] ?></li>
+</ul>
 
-<div class="blockform">
-	<h2><span><?php echo $lang_front['Delete post'] ?></span></h2>
-	<div class="box">
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?php printf($is_topic_post ? $lang_front['Topic by'] : $lang_front['Reply by'], '<strong>'.pun_htmlspecialchars($cur_post['poster']).'</strong>', format_time($cur_post['posted'])) ?></h3>
+    </div>
+	<div class="panel-body">
 		<form method="post" action="delete.php?id=<?php echo $id ?>">
-			<div class="inform">
-				<div class="forminfo">
-					<h3><span><?php printf($is_topic_post ? $lang_front['Topic by'] : $lang_front['Reply by'], '<strong>'.pun_htmlspecialchars($cur_post['poster']).'</strong>', format_time($cur_post['posted'])) ?></span></h3>
-					<p><?php echo ($is_topic_post) ? '<strong>'.$lang_front['Topic warning'].'</strong>' : '<strong>'.$lang_front['Warning'].'</strong>' ?><br /><?php echo $lang_front['Delete info'] ?></p>
-				</div>
-			</div>
-			<p class="buttons"><input type="submit" name="delete" value="<?php echo $lang_front['Delete'] ?>" /> <a href="javascript:history.go(-1)"><?php echo $lang_common['Go back'] ?></a></p>
+            <p><?php echo ($is_topic_post) ? '<strong>'.$lang_front['Topic warning'].'</strong>' : '<strong>'.$lang_front['Warning'].'</strong>' ?><br /><?php echo $lang_front['Delete info'] ?></p>
+			<div><input type="submit" class="btn btn-danger" name="delete" value="<?php echo $lang_front['Delete'] ?>" /> <a href="javascript:history.go(-1)" class="btn btn-default"><?php echo $lang_common['Go back'] ?></a></div>
 		</form>
 	</div>
 </div>
 
-<div id="postreview">
-	<div class="blockpost">
-		<div class="box">
-			<div class="inbox">
-				<div class="postbody">
-					<div class="postleft">
-						<dl>
-							<dt><strong><?php echo pun_htmlspecialchars($cur_post['poster']) ?></strong></dt>
-							<dd><span><?php echo format_time($cur_post['posted']) ?></span></dd>
-						</dl>
-					</div>
-					<div class="postright">
-						<div class="postmsg">
-							<?php echo $cur_post['message']."\n" ?>
-						</div>
-					</div>
-				</div>
-				<div class="clearer"></div>
-			</div>
-		</div>
-	</div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?php echo pun_htmlspecialchars($cur_post['poster']) ?></h3>
+    </div>
+	<div class="panel-body">
+		<?php echo $cur_post['message'] ?>
+    </div>
 </div>
 <?php
 
