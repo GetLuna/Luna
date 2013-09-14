e7d6a60ef75f5281e055a0a7dfbf09c64bb5b395
Yannick <contact@studio384.be>
2013-09-14 11:49:24 +0200

    Tag ModernBB 2.0.0

--- db_update.php
+++ db_update.php
@@ -8,7 +8,7 @@
  */
 
 // The ModernBB version this script updates to
-define('UPDATE_TO', '2.0-rc.3');
+define('UPDATE_TO', '2.0.0');
 
 define('UPDATE_TO_DB_REVISION', 32);
 define('UPDATE_TO_SI_REVISION', 2);
