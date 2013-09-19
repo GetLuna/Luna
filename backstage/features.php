1c41050e1cdafb9ed6a312851a095baeb9a58a2b
Yannick <contact@studio384.be>
2013-09-19 20:17:32 +0200

    #182 #208 Update features page

--- backstage/features.php
+++ backstage/features.php
@@ -24,18 +24,15 @@ require FORUM_ROOT.'lang/'.$admin_language.'/backstage.php';
 if (isset($_POST['form_sent']))
 {
 	$form = array(
-		'quickpost'				=> $_POST['form']['quickpost'] != '1' ? '0' : '1',
-		'users_online'			=> $_POST['form']['users_online'] != '1' ? '0' : '1',
-		'censoring'				=> $_POST['form']['censoring'] != '1' ? '0' : '1',
-		'signatures'			=> $_POST['form']['signatures'] != '1' ? '0' : '1',
-		'ranks'					=> $_POST['form']['ranks'] != '1' ? '0' : '1',
-		'topic_views'			=> $_POST['form']['topic_views'] != '1' ? '0' : '1',
-		'gzip'					=> $_POST['form']['gzip'] != '1' ? '0' : '1',
-		'search_all_forums'		=> $_POST['form']['search_all_forums'] != '1' ? '0' : '1',
+		'quickpost'				=> $_POST['form']['quickpost'] ? '1' : '0',
+		'users_online'			=> $_POST['form']['users_online'] ? '1' : '0',
+		'censoring'				=> $_POST['form']['censoring'] ? '1' : '0',
+		'signatures'			=> $_POST['form']['signatures'] ? '1' : '0',
+		'ranks'					=> $_POST['form']['ranks'] ? '1' : '0',
+		'topic_views'			=> $_POST['form']['topic_views'] ? '1' : '0',
+		'gzip'					=> $_POST['form']['gzip'] ? '1' : '0',
+		'search_all_forums'		=> $_POST['form']['search_all_forums'] ? '1' : '0',
 	);
-
-	if ($form['additional_navlinks'] != '')
-		$form['additional_navlinks'] = pun_trim(pun_linebreaks($form['additional_navlinks']));
 
 	foreach ($form as $key => $input)
 	{
@@ -67,85 +64,41 @@ require FORUM_ROOT.'backstage/header.php';
 generate_admin_menu('features');
 
 ?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?php echo $lang_back['Features head'] ?></h3>
-    </div>
-	<div class="panel-body">
-        <form method="post" action="features.php">
+<form method="post" action="features.php">
+    <h2><?php echo $lang_back['Features head'] ?></h2>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">General</h3>
+        </div>
+        <div class="panel-body">
             <input type="hidden" name="form_sent" value="1" />
             <fieldset>
-                <table class="table">
-                    <tr>
-                        <th class="col-xs-2"><?php echo $lang_back['Quick post label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[quickpost]" value="1"<?php if ($pun_config['o_quickpost'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[quickpost]" value="0"<?php if ($pun_config['o_quickpost'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php echo $lang_back['Quick post help'] ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><?php echo $lang_back['Users online label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[users_online]" value="1"<?php if ($pun_config['o_users_online'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[users_online]" value="0"<?php if ($pun_config['o_users_online'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php echo $lang_back['Users online help'] ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><a name="censoring"></a><?php echo $lang_back['Censor words label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[censoring]" value="1"<?php if ($pun_config['o_censoring'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[censoring]" value="0"<?php if ($pun_config['o_censoring'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php printf($lang_back['Censor words help'], '<a href="admin_censoring.php">'.$lang_back['Censoring'].'</a>') ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><a name="signatures"></a><?php echo $lang_back['Signatures label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[signatures]" value="1"<?php if ($pun_config['o_signatures'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[signatures]" value="0"<?php if ($pun_config['o_signatures'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php echo $lang_back['Signatures help'] ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><a name="ranks"></a><?php echo $lang_back['User ranks label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[ranks]" value="1"<?php if ($pun_config['o_ranks'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[ranks]" value="0"<?php if ($pun_config['o_ranks'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php printf($lang_back['User ranks help'], '<a href="admin_ranks.php">'.$lang_back['Ranks'].'</a>') ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><?php echo $lang_back['Topic views label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[topic_views]" value="1"<?php if ($pun_config['o_topic_views'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[topic_views]" value="0"<?php if ($pun_config['o_topic_views'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php echo $lang_back['Topic views help'] ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><?php echo $lang_back['GZip label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[gzip]" value="1"<?php if ($pun_config['o_gzip'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[gzip]" value="0"<?php if ($pun_config['o_gzip'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php echo $lang_back['GZip help'] ?></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><?php echo $lang_back['Search all label'] ?></th>
-                        <td>
-                            <label class="conl"><input type="radio" name="form[search_all_forums]" value="1"<?php if ($pun_config['o_search_all_forums'] == '1') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['Yes'] ?></strong></label>
-                            <label class="conl"><input type="radio" name="form[search_all_forums]" value="0"<?php if ($pun_config['o_search_all_forums'] == '0') echo ' checked="checked"' ?> />&#160;<strong><?php echo $lang_back['No'] ?></strong></label>
-                            <span class="help-block"><?php echo $lang_back['Search all help'] ?></span>
-                        </td>
-                    </tr>
-                </table>
+            	<h4>Topics and posts</h4>
+				<input type="checkbox" name="form[quickpost]" value="1" <?php if ($pun_config['o_quickpost'] == '1') echo ' checked="checked"' ?> /> <?php echo $lang_back['Quick post help'] ?><br />
+				<input type="checkbox" name="form[censoring]" value="1" <?php if ($pun_config['o_censoring'] == '1') echo ' checked="checked"' ?> /> <?php  printf($lang_back['Censor words help'], '<a href="admin_censoring.php">'.$lang_back['Censoring'].'</a>') ?><br />
+				<input type="checkbox" name="form[topic_views]" value="1" <?php if ($pun_config['o_topic_views'] == '1') echo ' checked="checked"' ?> /> <?php echo $lang_back['Topic views help'] ?><br /><br />
+				<h4>User features</h4>
+				<input type="checkbox" name="form[users_online]" value="1" <?php if ($pun_config['o_users_online'] == '1') echo ' checked="checked"' ?> /> <?php echo $lang_back['Users online help'] ?><br />
+				<input type="checkbox" name="form[signatures]" value="1" <?php if ($pun_config['o_signatures'] == '1') echo ' checked="checked"' ?> /> <?php echo $lang_back['Signatures help'] ?><br />
+				<input type="checkbox" name="form[ranks]" value="1" <?php if ($pun_config['o_ranks'] == '1') echo ' checked="checked"' ?> /> <?php printf($lang_back['User ranks help'], '<a href="admin_ranks.php">'.$lang_back['Ranks'].'</a>') ?><br /><br />
+                <h4>Others</h4>
+				<input type="checkbox" name="form[search_all_forums]" value="1" <?php if ($pun_config['o_search_all_forums'] == '1') echo ' checked="checked"' ?> /> <?php echo $lang_back['Search all help'] ?>
             </fieldset>
-            <p class="control-group"><input class="btn btn-primary" type="submit" name="save" value="<?php echo $lang_back['Save changes'] ?>" /></p>
-        </form>
+        </div>
     </div>
-</div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Advanced</h3>
+        </div>
+        <div class="panel-body">
+            <input type="hidden" name="form_sent" value="1" />
+            <fieldset>
+				<input type="checkbox" name="form[gzip]" value="1" <?php if ($pun_config['o_gzip'] == '1') echo ' checked="checked"' ?> /> <?php echo $lang_back['GZip help'] ?><br />
+            </fieldset>
+        </div>
+    </div>
+	<div class="alert alert-info"><input class="btn btn-primary" type="submit" name="save" value="<?php echo $lang_back['Save changes'] ?>" /></div>
+</form>
 <?php
 
 require FORUM_ROOT.'backstage/footer.php';
