7a876a34b7c34ddbb54bda461012fdef216bc5cc
Yannick <contact@studio384.be>
2013-11-18 19:57:49 +0100

    Misc update

--- misc.php
+++ misc.php
@@ -109,7 +109,7 @@ else if (isset($_GET['email']))
 		// Here we use strlen() not pun_strlen() as we want to limit the post to FORUM_MAX_POSTSIZE bytes, not characters
 		else if (strlen($message) > FORUM_MAX_POSTSIZE)
 			message($lang['Too long email message']);
-			
+
 		if ($pun_user['last_email_sent'] != '' && (time() - $pun_user['last_email_sent']) < $pun_user['g_email_flood'] && (time() - $pun_user['last_email_sent']) >= 0)
 			message(sprintf($lang['Email flood'], $pun_user['g_email_flood'], $pun_user['g_email_flood'] - (time() - $pun_user['last_email_sent'])));
 
