22a13816b1cfebb5c15a603aaba0e52dbe005c57
Yannick <contact@studio384.be>
2013-12-19 20:54:34 +0100

    #420 Remove Email label, is already a placeholder

--- login.php
+++ login.php
@@ -209,9 +209,8 @@ if (!empty($errors))
 <form class="form" id="request_pass" method="post" action="login.php?action=forget_2" onsubmit="this.request_pass.disabled=true;if(process_form(this)){return true;}else{this.request_pass.disabled=false;return false;}">
     <h1 class="form-heading"><?php echo $lang['Request pass'] ?></h1>
     <fieldset>
-        <h4><?php echo $lang['Email'] ?></h4>
         <input type="hidden" name="form_sent" value="1" />
-        <label class="required"><input class="form-control full-form" type="text" name="req_email" placeholder="<?php echo $lang['Email'] ?>" /></label>
+        <label class="required"><input class="form-control" type="text" name="req_email" placeholder="<?php echo $lang['Email'] ?>" /></label>
         <div class="pull-right" style="margin-top: 60px;">
             <?php if (empty($errors)): ?><a class="btn" href="javascript:history.go(-1)"><?php echo $lang['Go back'] ?></a><?php endif; ?><input class="btn btn-primary" type="submit" name="request_pass" value="<?php echo $lang['Submit'] ?>" />
         </div>
