1ffead278525f158540706546350e1412802e4b5
Yannick <contact@studio384.be>
2014-04-28 17:20:09 +0200

    No reason no notice

--- include/common.php
+++ include/common.php
@@ -58,7 +58,7 @@ if (!defined('FORUM'))
 $luna_start = get_microtime();
 
 // Make sure PHP reports all errors except E_NOTICE. ModernBB supports E_ALL, but a lot of scripts it may interact with, do not
-error_reporting(E_ALL);
+error_reporting(E_ALL ^ E_NOTICE);
 
 // Force POSIX locale (to prevent functions such as strtolower() from messing up UTF-8 strings)
 setlocale(LC_CTYPE, 'C');
