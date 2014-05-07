990916c8cf08d81d3410a20db8551d96754f254b
Yannick <contact@studio384.be>
2014-05-07 21:10:30 +0200

    #724 Add Pinkie

---
+++ style/Pinkie/information.php
@@ -0,0 +1,25 @@
+<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE extension SYSTEM "ext-1.0.dtd">
+
+<!--
+
+/**
+ * Copyright (C) 2014 ModernBB Group
+ * Licensed under GPLv3
+ */
+-->
+
+<style engine="1.0">
+	<id>pinkie</id>
+	<name>Pinkie</name>
+	<developer>ModernBB Group</developer>
+	<date>7 May 2014</date>
+	<version>3.0-dev</version>
+	<description>As fancy as can be, Pinkie is a style designed to be great and simple. Make it look pink, my friend!</description>
+	<minversion>3.3</minversion>
+	<maxversion>3.3</maxversion>
+</style>
+XML;
+?> 
