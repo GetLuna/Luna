e678c63c9dcbab242d19c79d339bc62cacc73ba9
Yannick <contact@studio384.be>
2014-05-15 21:11:37 +0200

    Rename Vanellope to Shy

--- style/Shy/information.php
+++ style/Shy/information.php
@@ -12,12 +12,12 @@ $xmlstr = <<<XML
 -->
 
 <style engine="1.0">
-	<id>vanellope</id>
-	<name>Vanellope</name>
-	<developer>ModernBB Group</developer>
-	<date>7 May 2014</date>
-	<version>3.0-dev</version>
-	<description>As fancy as can be, Vanellope is a style designed to be great and simple. Make it look Aquamarine, my friend!</description>
+	<id>shy</id>
+	<name>Shy</name>
+	<developer>Studio 384</developer>
+	<date>15 May 2014</date>
+	<version>3.0-beta</version>
+	<description>As fancy as can be, Shy is a style designed to be great and simple. Make it look Aquamarine, my friend!</description>
 	<minversion>3.3</minversion>
 	<maxversion>3.3</maxversion>
 </style>
