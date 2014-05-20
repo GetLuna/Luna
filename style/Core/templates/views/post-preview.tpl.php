f9a91de31ac0cdfbb1be8afeffb1e67851dda6ec
Yannick <contact@studio384.be>
2014-05-20 19:47:38 +0200

    #533 Update post-preview template

--- style/Core/templates/views/post-preview.tpl.php
+++ style/Core/templates/views/post-preview.tpl.php
@@ -9,11 +9,28 @@ if (!defined('FORUM'))
 
 ?>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><?php echo $lang['Post preview'] ?></h3>
+<div class="postview">
+    <div class="row topic">
+        <div class="col-md-3">
+            <div class="profile-card">
+                <div class="profile-card-head profile-card-quickpost">
+                    <div class="user-avatar thumbnail <?php if (!$user_avatar) echo 'noavatar'?>">
+                        <?php if ($user_avatar != '') echo "\t\t\t\t\t\t".$user_avatar."\n"; ?>
+                    </div>
+                    <h2 <?php if (!$user_avatar) echo 'class="noavatar"'; ?>><?php echo $username ?></h2>
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-            <?php echo $preview_message."\n" ?>
-        </div>
-    </div>
+        <div class="col-md-9">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<div class="comment-arrow hidden-sm hidden-xs"></div>
+					<h3 class="panel-title"><?php echo $lang['Post preview'] ?></h3>
+				</div>
+				<div class="panel-body">
+					<?php echo $preview_message."\n" ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
