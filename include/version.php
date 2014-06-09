4b8975e6f1d3eb4d85332a53bf3b8bb06984cec6
Yannick <contact@studio384.be>
2014-06-09 21:24:07 +0200

    Version bump

--- include/version.php
+++ include/version.php
@@ -9,7 +9,7 @@
 class Version
 {
 	// See http://modernbb.be/docs/version.php for more info
-	const FORUM_VERSION = '3.3.9.10';
+	const FORUM_VERSION = '3.4-alpha.2227';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = 68;
