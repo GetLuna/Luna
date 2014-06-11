34c86cf1f0f551624b7df2d45704cb0ef8d1c680
Yannick <contact@studio384.be>
2014-06-11 18:23:56 +0200

    Update version to reflect changes

--- include/version.php
+++ include/version.php
@@ -9,7 +9,7 @@
 class Version
 {
 	// See http://modernbb.be/docs/version.php for more info
-	const FORUM_VERSION = '3.4-alpha.2236';
+	const FORUM_VERSION = '3.4-alpha.2244';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = 68;
