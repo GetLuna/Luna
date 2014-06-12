0ec6d0d5d6fa6d876b97aa6abf709cd2c9927446
Yannick <contact@studio384.be>
2014-06-12 11:30:37 +0200

    #771 Don't display a border arround videos

--- include/parser.php
+++ include/parser.php
@@ -789,14 +789,14 @@ function do_bbcode($text, $is_signature = false)
 	$replace[] = '<sub>$1</sub>';
 
 	// DailyMotion videos
-	$replace[] = '<iframe width="640" height="360" src="http://www.dailymotion.com/embed/video/$3"></iframe>';
-	$replace[] = '<iframe width="$1" height="$2" src="http://www.dailymotion.com/embed/video/$5"></iframe>';
+	$replace[] = '<iframe width="640" frameborder="0" height="360" src="http://www.dailymotion.com/embed/video/$3"></iframe>';
+	$replace[] = '<iframe width="$1" frameborder="0" height="$2" src="http://www.dailymotion.com/embed/video/$5"></iframe>';
 	// Youtube Videos
-	$replace[] = '<iframe width="640" height="360" src="http://www.youtube.com/embed/$4"></iframe>';
-	$replace[] = '<iframe width="$1" height="$2" src="http://www.youtube.com/embed/$6"></iframe>';
+	$replace[] = '<iframe width="640" frameborder="0" height="360" src="http://www.youtube.com/embed/$4"></iframe>';
+	$replace[] = '<iframe width="$1" frameborder="0" height="$2" src="http://www.youtube.com/embed/$6"></iframe>';
 	// Vimeo Videos
-	$replace[] = '<iframe width="640" height="360" src="http://player.vimeo.com/video/$4"></iframe>';
-	$replace[] = '<iframe width="$1" height="$2" src="http://player.vimeo.com/video/$6"></iframe>';
+	$replace[] = '<iframe width="640" frameborder="0" height="360" src="http://player.vimeo.com/video/$4"></iframe>';
+	$replace[] = '<iframe width="$1" frameborder="0" height="$2" src="http://player.vimeo.com/video/$6"></iframe>';
 
 	if (($is_signature && $luna_config['p_sig_img_tag'] == '1') || (!$is_signature && $luna_config['p_message_img_tag'] == '1'))
 	{
