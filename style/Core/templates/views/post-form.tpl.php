4bd05ac6f7dbbd1e9f9fdb7ebc922857801c227c
Yannick <contact@studio384.be>
2014-06-25 22:38:59 +0200

    #817 Rewrite whole Javascript to be much simpeler

--- style/Core/templates/views/post-form.tpl.php
+++ style/Core/templates/views/post-form.tpl.php
@@ -34,33 +34,32 @@ if ($fid): ?>
 <?php endif; ?>
             <div class="btn-toolbar textarea-toolbar">
                 <div class="btn-group">
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[b][/b]');"><span class="fa fa-bold fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[u][/u]');"><span class="fa fa-underline fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[i][/i]');"><span class="fa fa-italic fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[s][/s]');"><span class="fa fa-strikethrough fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('b');"><span class="fa fa-bold fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('u');"><span class="fa fa-underline fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('i');"><span class="fa fa-italic fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('s');"><span class="fa fa-strikethrough fa-fw"></span></a>
                 </div>
                 <div class="btn-group">
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[h][/h]');"><span class="fa fa-header fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[sub][/sub]');"><span class="fa fa-subscript fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[sup][/sup]');"><span class="fa fa-superscript fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('h');"><span class="fa fa-header fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('sub');"><span class="fa fa-subscript fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('sup');"><span class="fa fa-superscript fa-fw"></span></a>
                 </div>
                 <div class="btn-group hidden-xs">
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[quote][/quote]');"><span class="fa fa-quote-left fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[code][/code]');"><span class="fa fa-code fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[c][/c]');"><span class="fa fa-file-code-o fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('quote');"><span class="fa fa-quote-left fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('code');"><span class="fa fa-code fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('c');"><span class="fa fa-file-code-o fa-fw"></span></a>
                 </div>
                 <div class="btn-group hidden-xs">
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[url][/url]');"><span class="fa fa-link fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[img][/img]');"><span class="fa fa-image fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[video][/video]');"><span class="fa fa-play-circle fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('url');"><span class="fa fa-link fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('img');"><span class="fa fa-image fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('video');"><span class="fa fa-play-circle fa-fw"></span></a>
                 </div>
                 <div class="btn-group hidden-xs">
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[list][/list]');"><span class="fa fa-list-ol fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[list=a][/list]');"><span class="fa fa-list-ul fa-fw"></span></a>
-                    <a class="btn btn-default" href="javascript:void(0);" onclick="inyectarTexto('req_message','[*][/*]');"><span class="fa fa-asterisk fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('list');"><span class="fa fa-list-ul fa-fw"></span></a>
+                    <a class="btn btn-default" href="javascript:void(0);" onclick="AddTag('*');"><span class="fa fa-asterisk fa-fw"></span></a>
                 </div>
             </div>
-            <textarea class="form-control"  placeholder="Start typing..." name="req_message" rows="20" tabindex="<?php echo $cur_index++ ?>"><?php echo isset($_POST['req_message']) ? luna_htmlspecialchars($orig_message) : (isset($quote) ? $quote : ''); ?></textarea>
+            <textarea class="form-control"  placeholder="Start typing..." name="req_message" id="post_field" rows="20" tabindex="<?php echo $cur_index++ ?>"><?php echo isset($_POST['req_message']) ? luna_htmlspecialchars($orig_message) : (isset($quote) ? $quote : ''); ?></textarea>
         </fieldset>
         <div class="panel-footer">
             <div class="btn-group"><input class="btn btn-primary" type="submit" name="submit" value="<?php echo $lang['Submit'] ?>" tabindex="<?php echo $cur_index++ ?>" accesskey="s" /><input class="btn btn-default" type="submit" name="preview" value="<?php echo $lang['Preview'] ?>" tabindex="<?php echo $cur_index++ ?>" accesskey="p" /></div><div class="btn-group"><a class="btn btn-link" href="javascript:history.go(-1)"><?php echo $lang['Go back'] ?></a></div>
@@ -127,22 +126,13 @@ if (!empty($checkboxes))
     </div>
 </form>
 <script>
-function inyectarTexto(elemento,valor) {
-     var elemento_dom=document.getElementsByName(elemento)[0];
-     if(document.selection) {
-         elemento_dom.focus();
-         sel=document.selection.createRange();
-         sel.text=valor;
-         return;
-     } if(elemento_dom.selectionStart||elemento_dom.selectionStart=="0") {
-         var t_start=elemento_dom.selectionStart;
-         var t_end=elemento_dom.selectionEnd;
-         var val_start=elemento_dom.value.substring(0,t_start);
-         var val_end=elemento_dom.value.substring(t_end,elemento_dom.value.length);
-         elemento_dom.value=val_start+valor+val_end;
-     } else {
-         elemento_dom.value+=valor;
-     }
+function AddTag(tag) {
+   var Field = document.getElementById('post_field');
+   var val = Field.value;
+   var selected_txt = val.substring(Field.selectionStart, Field.selectionEnd);
+   var before_txt = val.substring(0, Field.selectionStart);
+   var after_txt = val.substring(Field.selectionEnd, val.length);
+   Field.value = before_txt + '[' + tag + ']' + selected_txt + '[/' + tag + ']' + after_txt;
 }
 </script>
 <?php
