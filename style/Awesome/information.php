4f3b9666a1554af4843333b621193f74add57f17
Yannick <contact@studio384.be>
2014-07-07 14:19:46 +0200

    Update style information

--- style/Awesome/information.php
+++ style/Awesome/information.php
@@ -15,10 +15,10 @@ $xmlstr = <<<XML
 	<id>awesome</id>
 	<name>Awesome</name>
 	<developer>Studio 384</developer>
-	<date>1 July 2014</date>
-	<version>3.1.1-dev</version>
+	<date>8 July 2014</date>
+	<version>3.1.1</version>
 	<description>As fancy as can be, Awesome is a style designed to be great and simple. Make it look green, my friend!</description>
-	<minversion>3.4.1-dev</minversion>
+	<minversion>3.4.1</minversion>
 	<maxversion>3.4.1</maxversion>
 </style>
 XML;
