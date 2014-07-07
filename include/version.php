fb61a083cd96e3ab160605c8cb84c56d29adf4d6
Yannick <contact@studio384.be>
2014-07-07 14:25:29 +0200

    Final bugfix for 3.4.1

--- include/version.php
+++ include/version.php
@@ -9,7 +9,7 @@
 class Version
 {
 	// See http://modernbb.be/docs/version.php for more info
-	const FORUM_VERSION = '3.4.0.37';
+	const FORUM_VERSION = '3.4.0.38';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = 75;
