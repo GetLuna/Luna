adef06f3606152df96af5dd2cec9fb6547aea09f
Yannick <contact@studio384.be>
2014-08-03 11:00:35 +0200

    #844 Redesign search results when as posts

--- style/Core/templates/views/search-show_as_posts.tpl.php
+++ style/Core/templates/views/search-show_as_posts.tpl.php
@@ -34,31 +34,8 @@ if (!defined('FORUM'))
                 }
 ?>
 
-<div class="blockpost<?php echo ($post_count % 2 == 0) ? ' roweven' : ' rowodd' ?><?php if ($cur_search['pid'] == $cur_search['first_post_id']) echo ' firstpost' ?><?php if ($post_count == 1) echo ' blockpost1' ?><?php if ($item_status != '') echo ' '.$item_status ?>">
-    <table class="table">
-        <tr>
-            <td class="col-lg-2 user-data">
-                <?php echo $pposter ?><br />
-                <?php if ($cur_search['pid'] == $cur_search['first_post_id']) : ?>
-                    <?php echo $lang['Replies'].' '.forum_number_format($cur_search['num_replies']) ?>
-                <?php endif; ?>
-            </td>
-            <td class="col-lg-10">
-                <?php echo $message."\n" ?>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2" class="postfooter" style="padding-bottom: 0;">
-                <div class="btn-group btn-breadcrumb">
-                    <?php echo $forum ?>
-                    <a class="btn btn-primary" href="viewtopic.php?id=<?php echo $cur_search['tid'] ?>"><?php echo luna_htmlspecialchars($cur_search['subject']) ?></a>
-                    <a class="btn btn-primary" href="viewtopic.php?pid=<?php echo $cur_search['pid'].'#p'.$cur_search['pid'] ?>"><?php if ($cur_search['pid'] != $cur_search['first_post_id']) echo $lang['Re'].' ' ?><?php echo format_time($cur_search['pposted']) ?></a>
-                </div>
-                <p class="pull-right">
-                    <a class="btn btn-small btn-primary" href="viewtopic.php?id=<?php echo $cur_search['tid'] ?>"><?php echo $lang['Go to topic'] ?></a>
-                    <a class="btn btn-small btn-primary" href="viewtopic.php?pid=<?php echo $cur_search['pid'].'#p'.$cur_search['pid'] ?>"><?php echo $lang['Go to post'] ?></a>
-                </p>
-            </td>
-        </tr>
-    </table>
+<div class="result-card blockpost<?php echo ($post_count % 2 == 0) ? ' roweven' : ' rowodd' ?><?php if ($cur_search['pid'] == $cur_search['first_post_id']) echo ' firstpost' ?><?php if ($post_count == 1) echo ' blockpost1' ?><?php if ($item_status != '') echo ' '.$item_status ?>">
+	<h3><a href="viewtopic.php?id=<?php echo $cur_search['tid'] ?>"><?php echo luna_htmlspecialchars($cur_search['subject']) ?></a><small> by <?php echo $pposter ?></small></h3>
+    <span class="help-block"><?php echo $forum ?> > <a href="viewtopic.php?id=<?php echo $cur_search['tid'] ?>"><?php echo luna_htmlspecialchars($cur_search['subject']) ?></a> > <a href="viewtopic.php?pid=<?php echo $cur_search['pid'].'#p'.$cur_search['pid'] ?>"><?php if ($cur_search['pid'] != $cur_search['first_post_id']) echo $lang['Re'].' ' ?><?php echo format_time($cur_search['pposted']) ?></a></span>
+    <?php echo $message."\n" ?>
 </div>
