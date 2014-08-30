05db991f0d9c9933d51aed3d8ecff81dd43054db
Yannick <contact@studio384.be>
2014-08-30 20:19:29 +0200

    #900 May development begin

---
+++ style/Sunrise/information.php
@@ -0,0 +1,25 @@
+<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE extension SYSTEM "ext-1.0.dtd">
+
+<!--
+
+/**
+ * Copyright (C) 2014 Studio 384
+ * Licensed under GPLv3 (http://modernbb.be/license.php)
+ */
+-->
+
+<style engine="1.0">
+	<id>sunrise</id>
+	<name>Sunrise</name>
+	<developer>Studio 384</developer>
+	<date>30 August 2014</date>
+	<version>1.0-dev</version>
+	<description>Sunrise is the default theme of the Luna software, this modern, fresh, yet colorfull design makes your board an unique piece!</description>
+	<minversion>0.0</minversion>
+	<maxversion>1.0</maxversion>
+</style>
+XML;
+?>
