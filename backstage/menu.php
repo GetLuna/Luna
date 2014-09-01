dc2e8f6b1315a5ee76890b335c861681ccd4b99d
Yannick <contact@studio384.be>
2014-09-01 21:25:38 +0200

    #865 List menu items in Backstage

--- backstage/menu.php
+++ backstage/menu.php
@@ -10,6 +10,8 @@ require '../include/common.php';
 
 if (!$luna_user['is_admmod'])
     header("Location: ../login.php");
+	
+$result = $db->query('SELECT id, url, name, disp_position, disp, sys_entry FROM '.$db->prefix.'menu ORDER BY disp_position') or error('Unable to fetch menu items', __FILE__, __LINE__, $db->error());
 
 require 'header.php';
 load_admin_nav('settings', 'menu');
@@ -21,8 +23,34 @@ load_admin_nav('settings', 'menu');
 			<div class="panel-heading">
 				<h3 class="panel-title">Menu</h3>
 			</div>
-			<div class="panel-body">
-			</div>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>URL</th>
+						<th>Position</th>
+						<th>Show</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+<?php
+if ($db->num_rows($result) > 0) {
+	while ($cur_item = $db->fetch_assoc($result)) {
+?>
+					<tr>
+						<td><?php echo $cur_item['name'] ?></td>
+						<td><?php echo $cur_item['url'] ?></td>
+						<td><?php echo $cur_item['disp_position'] ?></td>
+						<td><?php echo $cur_item['disp'] ?></td>
+						<td><?php echo $cur_item['sys_entry'] ?></td>
+					</tr>
+<?php
+	}
+}
+?>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
