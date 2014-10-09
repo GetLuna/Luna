273394a8dcd87b7b691e15ea61a921fb0556ebb5
Yannick <contact@studio384.be>
2014-10-09 19:25:15 +0200

    #905 Remove navigation, we might add it in the future anyway

--- style/Sunrise/objects/navigation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Make sure no one attempts to run this view directly.
-if (!defined('FORUM'))
-    exit;
-
-?>
-
-<a class="btn btn-primary" href="viewtopic.php?id=<?php echo $cur_post['tid'] ?>"><span class="fa fa-chevron-left"></span> <?php echo luna_htmlspecialchars($cur_post['subject']) ?></a>
