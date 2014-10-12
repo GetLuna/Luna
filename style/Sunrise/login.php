9eb21700101cf15c088d4409f166a6d8b90b949a
Yannick <contact@studio384.be>
2014-10-12 20:54:54 +0200

    #905 Add password request modal, update login modal

--- style/Sunrise/login.php
+++ style/Sunrise/login.php
@@ -9,22 +9,46 @@
 					<fieldset>
 						<input type="hidden" name="form_sent" value="1" />
 						<input type="hidden" name="redirect_url" value="<?php echo luna_htmlspecialchars($redirect_url) ?>" />
-						<div class="form-content">
-							<input class="form-control" type="text" name="req_username" maxlength="25" tabindex="1" placeholder="<?php echo $lang['Username'] ?>" />
-							<input class="form-control" type="password" name="req_password" tabindex="2" placeholder="<?php echo $lang['Password'] ?>" />
-							<div class="control-group">
-								<div class="controls remember">
-									<label class="remember"><input type="checkbox" name="save_pass" value="1" tabindex="3" checked="checked" /> <?php echo $lang['Remember me'] ?></label>
-								</div>
+						<input class="form-control" type="text" name="req_username" maxlength="25" tabindex="1" placeholder="<?php echo $lang['Username'] ?>" />
+						<input class="form-control" type="password" name="req_password" tabindex="2" placeholder="<?php echo $lang['Password'] ?>" />
+						<div class="control-group">
+							<div class="controls remember">
+								<label class="remember"><input type="checkbox" name="save_pass" value="1" tabindex="3" checked="checked" /> <?php echo $lang['Remember me'] ?></label>
 							</div>
-							<input class="btn btn-primary btn-block" type="submit" name="login" value="<?php echo $lang['Login'] ?>" tabindex="4" />
-							<hr />
-							<a class="btn btn-primary btn-block" href="register.php">Register</a>
-							<hr />
-							<p class="actions"><?php if ($luna_config['o_regs_allow'] == '1') { ?><a href="register.php" tabindex="5"><?php echo $lang['Register'] ?></a> &middot; <?php }; ?><a href="login.php?action=forget" tabindex="6"><?php echo $lang['Forgotten pass'] ?></a></p>
+						</div>
+						<input class="btn btn-primary btn-block" type="submit" name="login" value="<?php echo $lang['Login'] ?>" tabindex="4" />
+						<hr />
+						<a class="btn btn-primary btn-block" href="register.php">Register</a>
+					</fieldset>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<?php if ($luna_config['o_regs_allow'] == '1') { ?><a href="register.php" tabindex="5"><?php echo $lang['Register'] ?></a> &middot; <?php }; ?><a href="#" data-toggle="modal" data-target="#newpass" data-dismiss="modal" tabindex="6"><?php echo $lang['Forgotten pass'] ?></a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="newpass" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-xs">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Request password</h4>
+			</div>
+			<div class="modal-body">
+				<form id="request_pass" method="post" action="login.php?action=forget_2" onsubmit="this.request_pass.disabled=true;if(process_form(this)){return true;}else{this.request_pass.disabled=false;return false;}">
+					<fieldset>
+						<input type="hidden" name="form_sent" value="1" />
+						<div class="input-group">
+							<input class="form-control" type="text" name="req_email" placeholder="<?php echo $lang['Email'] ?>" />
+							<span class="input-group-btn">
+								<input class="btn btn-primary" type="submit" name="request_pass" value="<?php echo $lang['Submit'] ?>" />
+							</span>
 						</div>
 					</fieldset>
 				</form>
+			</div>
+			<div class="modal-footer">
+				<a href="#" data-toggle="modal" data-target="#login" data-dismiss="modal">Back</a>
 			</div>
 		</div>
 	</div>
