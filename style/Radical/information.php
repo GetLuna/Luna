db7b825de16237feffb75b410c7938f1af6db05e
Yannick <contact@studio384.be>
2014-10-13 19:24:23 +0200

    #921 Fix Quote list style issues

--- style/Radical/information.php
+++ style/Radical/information.php
@@ -15,11 +15,11 @@ $xmlstr = <<<XML
 	<id>radical</id>
 	<name>Radical</name>
 	<developer>Studio 384</developer>
-	<date>9 September 2014</date>
-	<version>3.2.0</version>
+	<date>13 October 2014</date>
+	<version>3.2.1</version>
 	<description>As fancy as can be, Awesome is a style designed to be great and simple. Make it look red, my friend!</description>
 	<minversion>3.4.1</minversion>
-	<maxversion>3.5.0</maxversion>
+	<maxversion>3.5.1</maxversion>
 </style>
 XML;
 ?> 
