d27de59fbf7460dae79f632e361e7f5281a16310
Yannick <contact@studio384.be>
2014-10-18 21:34:13 +0200

    Tag Luna 0.0.1

--- include/version.php
+++ include/version.php
@@ -8,10 +8,10 @@
 
 class Version {
 	// See http://modernbb.be/docs/version.php for more info
-	const FORUM_VERSION = '0.0.0';
+	const FORUM_VERSION = '0.0.1';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '0.0.3135';
+	const FORUM_CORE_VERSION = '0.0.3136';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = 84.25;
