c6e87bedd93d2e3b266f095a0c8ff71547eb2a1b
Yannick <contact@studio384.be>
2014-10-22 20:49:21 +0200

    Update First-run

--- style/Sunrise/index.php
+++ style/Sunrise/index.php
@@ -8,7 +8,11 @@ if (!defined('FORUM'))
 
 <div class="first-run panel panel-default">
     <div class="row first-run-content">
+        <?php if (!$luna_user['is_guest']) { ?>
         <div class="col-md-4 col-sm-6 first-run-profile">
+        <?php } else { ?>
+        <div class="col-md-8 col-sm-6 first-run-profile">
+        <?php } ?>
             <h3 class="first-run-title"><?php echo sprintf($lang['Hi there'], luna_htmlspecialchars($luna_user['username'])) ?></h3>
             <span class="first-run-avatar thumbnail">
                 <?php echo $user_avatar ?>
@@ -31,31 +35,10 @@ if (!defined('FORUM'))
         <div class="col-md-4 hidden-sm">
             <h3 class="first-run-forumtitle"><?php echo sprintf($lang['Welcome to'], $luna_config['o_board_title']) ?></h3>
             <div class="list-group first-run-list">
+                <a href="#" data-toggle="modal" data-target="#login" class="list-group-item"><?php echo $lang['Login'] ?></a>
                 <a href="register.php" class="list-group-item"><?php echo $lang['Register'] ?></a>
                 <a href="login.php?action=forget" class="list-group-item"><?php echo $lang['Forgotten pass'] ?></a>
             </div>
-        </div>
-        <div class="col-md-4 col-sm-6">
-            <form class="form form-first-run" id="login" method="post" action="login.php?action=in" onsubmit="return process_form(this)">
-                <fieldset>
-                    <h3><?php echo $lang['Login'] ?></h3>
-                    <input type="hidden" name="form_sent" value="1" />
-                    <div class="first-run-login">
-                        <input class="form-control top-form" type="text" name="req_username" maxlength="25" tabindex="1" placeholder="<?php echo $lang['Username'] ?>" />
-                        <input class="form-control bottom-form" type="password" name="req_password" tabindex="2" placeholder="<?php echo $lang['Password'] ?>" />
-                    </div>
-                    <div class="form-content">
-                        <div class="control-group">
-                            <div class="controls remember">
-                                <label class="remember"><input type="checkbox" name="save_pass" value="1" tabindex="3" checked="checked" /> <?php echo $lang['Remember me'] ?></label>
-                            </div>
-                        </div>
-                        <div class="control-group pull-right">
-                            <input class="btn btn-primary" type="submit" name="login" value="<?php echo $lang['Login'] ?>" tabindex="4" />
-                        </div>
-                    </div>
-                </fieldset>
-            </form>
         </div>
         <?php } ?>
     </div>
