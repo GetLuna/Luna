07cc7b971d7cf9177a68fcd1431aaf72e6ba7d80
Yannick <contact@studio384.be>
2014-10-29 20:35:59 +0100

    Add support for extensions to navbar, update navbar

--- include/backstage_functions.php
+++ include/backstage_functions.php
@@ -8,7 +8,7 @@
 //
 // Display the admin navigation menu
 //
-function load_admin_nav($section ='', $page = '') {
+function load_admin_nav($section, $page) {
 	global $luna_user, $lang;
 
 ?>
@@ -28,7 +28,28 @@ function load_admin_nav($section ='', $page = '') {
                 <li class="<?php if ($section == 'backstage') echo 'active'; ?>"><a href="index.php"><span class="fa fa-dashboard"></span> <?php echo $lang['Backstage'] ?></a></li>
                 <li class="<?php if ($section == 'content') echo 'active'; ?>"><a href="board.php"><span class="fa fa-file"></span> <?php echo $lang['Content'] ?></a></li>
                 <li class="<?php if ($section == 'users') echo 'active'; ?>"><a href="users.php"><span class="fa fa-users"></span> <?php echo $lang['Users'] ?></a></li>
-                <li class="<?php if ($section == 'settings') echo 'active'; ?>"><a href="settings.php"><span class="fa fa-cog"></span> <?php echo $lang['Settings'] ?></a></li>
+                <li class="<?php if ($section == 'settings') echo 'active'; ?>"><a href="settings.php"><span class="fa fa-cog"></span> <?php echo $lang['Settings'] ?></a></li>		
+<?php
+
+	// See if there are any plugins
+	$plugins = forum_list_plugins($is_admin);
+
+	// Did we find any plugins?
+	if (!empty($plugins))
+	{
+?>
+                <li class="dropdown<?php if ($section == 'extensions') echo 'active'; ?>">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <span class="fa fa-cogs"></span> <?php echo $lang['Extensions'] ?> <span class="fa fa-angle-down">
+                    </a>
+                    <ul class="dropdown-menu">
+<?php
+		foreach ($plugins as $plugin_name => $plugin)
+			echo "\t\t\t\t\t".'<li><a href="loader.php?plugin='.$plugin_name.'">'.str_replace('_', ' ', $plugin).'</a></li>'."\n";
+?>
+                    </ul>
+                </li>
+<?php } ?>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown usermenu">
@@ -51,50 +72,55 @@ function load_admin_nav($section ='', $page = '') {
         <div class="row">
             <h2>
                 <?php
-                    if($page == 'index')
+                    if ($page == 'index')
                         echo 'Backstage';
-                    if($page == 'stats')
+                    else if ($page == 'stats')
                         echo 'System info';
-                    if($page == 'update')
+                    else if ($page == 'update')
                         echo 'Luna software update';
 
-                    if($page == 'board')
+                    else if ($page == 'board')
                         echo 'Board structure';
-                    if($page == 'censoring')
+                    else if ($page == 'censoring')
                         echo 'Censoring';
-                    if($page == 'reports')
+                    else if ($page == 'reports')
                         echo 'Reports';
 
-                    if($page == 'users')
+                    else if ($page == 'users')
                         echo 'Users';
-                    if($page == 'ranks')
+                    else if ($page == 'ranks')
                         echo 'Ranks';
-                    if($page == 'groups')
+                    else if ($page == 'groups')
                         echo 'Groups';
-                    if($page == 'permissions')
+                    else if ($page == 'permissions')
                         echo 'Permissions';
-                    if($page == 'bans')
+                    else if ($page == 'bans')
                         echo 'Bans';
 
-                    if($page == 'settings')
+                    else if ($page == 'settings')
                         echo 'Settings';
-                    if($page == 'features')
+                    else if ($page == 'features')
                         echo 'Features';
-                    if($page == 'registration')
+                    else if ($page == 'registration')
                         echo 'Registration';
-                    if($page == 'email')
+                    else if ($page == 'email')
                         echo 'Email';
-                    if($page == 'appearance')
+                    else if ($page == 'appearance')
                         echo 'Appearance';
-                    if($page == 'menu')
+                    else if ($page == 'menu')
                         echo 'Menu';
-                    if($page == 'theme')
+                    else if ($page == 'theme')
                         echo 'Theme';
-                    if($page == 'maintenance')
+                    else if ($page == 'maintenance')
                         echo 'Maintenance';
+                    else if ($page == 'zsettings')
+                        echo 'zSettings';
 
-                    if($page == 'database')
+                    else if ($page == 'database')
                         echo 'Database management';
+						
+					else
+						echo $page;
 						
 					echo '<span class="pull-right" style="font-size: 70%;">Core '.Version::FORUM_CORE_VERSION.'</span>';
                 ?>
@@ -129,6 +155,9 @@ function load_admin_nav($section ='', $page = '') {
                 <li<?php if($page == 'menu') echo ' class="active"' ?>><a href="menu.php">Menu</a></li>
                 <li<?php if($page == 'theme') echo ' class="active"' ?>><a href="theme.php">Theme</a></li>
                 <li<?php if($page == 'maintenance') echo ' class="active"' ?>><a href="maintenance.php">Maintenance</a></li>
+				<?php if (file_exists('../z.txt')) { ?>
+                <li<?php if($page == 'zsettings') echo ' class="active"' ?>><a href="zsettings.php">zSettings</a></li>
+				<?php } ?>
             </ul>
             <?php } ?>
         </div>
