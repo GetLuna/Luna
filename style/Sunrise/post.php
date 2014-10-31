3453bc4019b391de42f1825a31b8a3c28a35d40f
Yannick <contact@studio384.be>
2014-10-31 13:40:09 +0100

    #900 Update posting interface

--- style/Sunrise/post.php
+++ style/Sunrise/post.php
@@ -4,11 +4,16 @@
 if (!defined('FORUM'))
     exit;
 
-?>
+$jumbo_style = 'style="background:'.$cur_posting['color'].';"';
 
-<div class="btn-group btn-breadcrumb">
-    <a class="btn btn-primary" href="viewforum.php?id=<?php echo $cur_posting['id'] ?>"><span class="fa fa-chevron-left"></span> <?php echo luna_htmlspecialchars($cur_posting['forum_name']) ?></a>
+?>
 </div>
+<div class="jumbotron<?php echo $item_status ?>"<?php echo $jumbo_style ?>>
+	<div class="container">
+		<h2>New topic in <?php echo luna_htmlspecialchars($cur_posting['forum_name']) ?></h2><span class="pull-right"><a class="btn btn-danger" href="viewforum.php?id=<?php echo $cur_posting['id'] ?>"><span class="fa fa-chevron-left"></span> Cancel</a></span>
+	</div>
+</div>
+<div class="container">
 <?php draw_error_panel($errors); ?>
 <?php draw_preview_panel($message); ?>
 
