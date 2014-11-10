dd1e50ea77dd5c5f7b8d3c446482eb7e49ac57b7
Yannick <contact@studio384.be>
2014-11-10 23:47:06 +0100

    #963 Allow parser to actualy understand

--- include/parser.php
+++ include/parser.php
@@ -803,8 +803,28 @@ function parse_message($text, $hide_smilies) {
 		foreach ($parts as $i => $part) {
 			$text .= $part;
 			if (isset($inside[$i])) {
-				$num_lines = (substr_count($inside[$i], "\n"));
-				$text .= '</p><div class="codebox"><pre'.(($num_lines > 28) ? ' class="vscroll"' : '').'><code>'.luna_trim($inside[$i], "\n\r").'</code></pre></div><p>';
+				$num_lines = (substr_count($inside[$i], "\n"));$code_line = explode("\n", $inside[$i]);
+				$first_line = trim($code_line[1]);
+				if (strpos($first_line, '[[') !== false && strpos($first_line, ']]') !== false) {
+					// fetching the language name
+					$language = strtolower(trim(str_replace(array('[[', ']]'), '', $first_line)));
+					// Markup case
+					if($language == 'html' || $language == 'xhtml' || $language == 'xml') { $h_class = ' class="language-markup"'; }
+					// C-like languages case
+					elseif($language == 'php' || $language == 'c++' || $language == 'perl') { $h_class = ' class="language-clike"'; }
+					// JavaScript case
+					elseif($language == 'javascript') { $h_class = ' class="language-javascript"'; }
+					// C-like languages case
+					elseif($language == 'php') { $h_class = ' class="language-php"'; }
+					// Other cases
+					else { $h_class = ' class="language-none"'; }
+					// Deleting the line giving the code name
+					$inside[$i] = str_replace($first_line, '', $inside[$i]);
+					// Generating the the HTML code block
+					$text .= '</p><div class="codebox"><pre'.(($num_lines > 28) ? ' class="vscroll"' : '').'><code'.$h_class.'>'.luna_trim($inside[$i], "\n\r").'</code></pre></div><p>';
+				} else {
+					$text .= '</p><div class="codebox"><pre'.(($num_lines > 28) ? ' class="vscroll"' : '').'><code>'.luna_trim($inside[$i], "\n\r").'</code></pre></div><p>';
+				}
 			}
 		}
 	}
