d73c1eb6d0dedc6b3f494a95d4ef01636ad881c7
Yannick <contact@studio384.be>
2014-12-05 15:13:22 +0100

    #870 Add heading

--- style/Sunrise/personality.php
+++ style/Sunrise/personality.php
@@ -45,6 +45,20 @@ if (!defined('FORUM'))
 
 ?>
 
+</div>
+<div class="jumbotron me-jumbotron">
+	<div class="container">
+        <div class="media">
+            <a class="pull-left" href="#">
+                <?php echo generate_avatar_markup($luna_user['id']) ?>
+            </a>
+            <div class="media-body">
+                <h2 class="media-heading"><?php echo $user['username']; ?></h2>
+            </div>
+        </div>
+	</div>
+</div>
+<div class="container">
 <div class="col-sm-3 profile-nav">
 <?php
     generate_me_menu('personality');
