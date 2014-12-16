7c6e903a37452a4a892ddd256079e40d207f17b7
Yannick <contact@studio384.be>
2014-12-16 21:02:51 +0100

    #863 Update version to 0.2-dev

--- include/version.php
+++ include/version.php
@@ -8,10 +8,10 @@
 
 class Version {
 	// See http://modernbb.be/docs/version.php for more info
-	const FORUM_VERSION = '0.1.9';
+	const FORUM_VERSION = '0.2-dev';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '0.2.3477';
+	const FORUM_CORE_VERSION = '0.2.3478';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = '87.00';
