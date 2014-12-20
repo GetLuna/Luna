57dcafcf4345346a50846809d0265df1ecd222d5
Yannick <contact@studio384.be>
2014-12-20 09:44:53 +0100

    #870 Remove old profile

--- include/functions.php
+++ include/functions.php
@@ -539,24 +539,6 @@ function update_users_online() {
 //
 // Display the profile navigation menu
 //
-function generate_profile_menu($page = '') {
-	global $lang, $luna_config, $luna_user, $id;
-
-?>
-<div class="list-group">
-    <a class="<?php if ($page == 'view') echo 'active'; ?> list-group-item" href="profile.php?section=view&amp;id=<?php echo $id ?>"><?php echo $lang['Profile'] ?></a>
-	<?php if ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1')): ?>
-		<a class="<?php if ($page == 'admin') echo 'active'; ?> list-group-item" href="profile.php?section=admin&amp;id=<?php echo $id ?>"><?php echo $lang['Section admin'] ?></a>
-	<?php endif; ?>
-</div>
-<?php
-
-}
-
-
-//
-// Display the profile navigation menu
-//
 function generate_me_menu($page = '') {
 	global $lang, $luna_config, $luna_user, $id;
 
