4ffed7f2c90443a3641c7cbb4507d997ad7c2952
Yannick <contact@studio384.be>
2014-12-20 14:23:59 +0100

    #1024 Add emojis settings

--- backstage/appearance.php
+++ backstage/appearance.php
@@ -107,6 +107,12 @@ if (isset($_GET['saved']))
                         </div>
                     </div>
                 </div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Emojis<span class="help-block">The font size emojis should be displayed in</span></label>
+                    <div class="col-sm-9">
+                        <input disabled type="text" class="form-control" name="form[emoji_size]" maxlength="2" value="<?php echo $luna_config['o_emoji_size'] ?>" />
+                    </div>
+                </div>
                 <hr />
                 <div class="form-group">
                     <label class="col-sm-3 control-label"><?php echo $lang['Topic review label'] ?><span class="help-block"><?php echo $lang['Topic review help'] ?></span></label>
