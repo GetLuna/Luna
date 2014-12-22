b3ff7bf771f9398700e86dced8427ea918a3e60a
Yannick <contact@studio384.be>
2014-12-22 20:56:23 +0100

    #1028 Include logics for profile settings

--- settings.php
+++ settings.php
@@ -12,10 +12,30 @@ require FORUM_ROOT.'include/parser.php';
 // Load the me functions script
 require FORUM_ROOT.'include/me_functions.php';
 
+$id = isset($_GET['id']) ? intval($_GET['id']) : $luna_user['id'];
+
+if ($luna_user['is_admmod']) {
+	if ($luna_user['g_id'] == FORUM_ADMIN || $luna_user['g_mod_rename_users'] == '1')
+		$username_field = '<input type="text" class="form-control" name="req_username" value="'.luna_htmlspecialchars($user['username']).'" maxlength="25" />';
+	else
+		$username_field = luna_htmlspecialchars($user['username']);
+
+	$email_field = '<input type="text" class="form-control" name="req_email" value="'.luna_htmlspecialchars($user['email']).'" maxlength="80" />';
+	$email_button = '<span class="input-group-btn"><a class="btn btn-primary" href="misc.php?email='.$id.'">'.$lang['Send email'].'</a></span>';
+} else {
+	$username_field = '<div class="col-sm-9"><input class="form-control" type="text"  value="'.luna_htmlspecialchars($user['username']).'" disabled="disabled" />';
+
+	if ($luna_config['o_regs_verify'] == '1') {
+		$email_field = '<input type="text" class="form-control" name="req_email" value="'.luna_htmlspecialchars($user['email']).'" maxlength="80" disabled />';
+		$email_button = '<span class="input-group-btn"><a class="btn btn-primary" href="#" data-toggle="modal" data-target="#newmail">'.$lang['Change email'].'</a></span>';
+	} else {
+		$email_field = '<input type="text" class="form-control" name="req_email" value="'.$user['email'].'" maxlength="80" />';
+		$email_button = '';
+	}
+}
 
 $page_title = array(luna_htmlspecialchars($luna_config['o_board_title']), $lang['Profile'], $lang['Settings']);
 define('FORUM_ACTIVE_PAGE', 'me');
 require load_page('header.php');
-
 require load_page('settings2.php');
 require load_page('footer.php');
