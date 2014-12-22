d1aca7ad4538409691cc8f4764502444c52ea82a
Yannick <contact@studio384.be>
2014-12-22 15:44:56 +0100

    #1028 Add menu to settings page

--- style/Sunrise/settings2.php
+++ style/Sunrise/settings2.php
@@ -26,5 +26,38 @@ if (!defined('FORUM'))
 </div>
 <div class="col-sm-9">
 <h2>Settings</h2>
-<h3>Under construction</h3>
+<div role="tabpanel">
+	<ul class="nav nav-tabs" role="tablist">
+		<li role="presentation" class="active"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+		<li role="presentation"><a href="#personalize" aria-controls="personalize" role="tab" data-toggle="tab">Personalize</a></li>
+		<li role="presentation"><a href="#email" aria-controls="email" role="tab" data-toggle="tab">Email</a></li>
+		<li role="presentation"><a href="#contact" aria-controls="contact" role="tab" data-toggle="tab">Contact</a></li>
+		<li role="presentation"><a href="#threads" aria-controls="threads" role="tab" data-toggle="tab">Threads</a></li>
+		<li role="presentation"><a href="#time" aria-controls="time" role="tab" data-toggle="tab">Time</a></li>
+		<li role="presentation"><a href="#admin" aria-controls="admin" role="tab" data-toggle="tab">Admin</a></li>
+	</ul>
+	<div class="tab-content">
+		<div role="tabpanel" class="tab-pane active" id="profile">
+			<h3>Under construction</h3>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="personalize">
+			<h3>Under construction</h3>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="email">
+			<h3>Under construction</h3>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="contact">
+			<h3>Under construction</h3>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="threads">
+			<h3>Under construction</h3>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="time">
+			<h3>Under construction</h3>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="admin">
+			<h3>Under construction</h3>
+		</div>
+	</div>
+</div>
 </div>
