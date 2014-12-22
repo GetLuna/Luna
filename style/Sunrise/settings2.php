261a111b4384b22e2acc7d1f32b0c02afa5a916c
Yannick <contact@studio384.be>
2014-12-22 15:39:14 +0100

    #1028 Add profile interface

--- style/Sunrise/settings2.php
+++ style/Sunrise/settings2.php
@@ -5,4 +5,26 @@ if (!defined('FORUM'))
     exit;
 
 ?>
+</div>
+<div class="jumbotron me-jumbotron">
+	<div class="container">
+        <div class="media">
+            <a class="pull-left" href="#">
+                <?php echo draw_user_avatar($luna_user['id'], 'avatar-me') ?>
+            </a>
+            <div class="media-body">
+                <h2 class="media-heading"><?php echo $user['username']; ?></h2>
+            </div>
+        </div>
+	</div>
+</div>
+<div class="container">
+<div class="col-sm-3 profile-nav">
+<?php
+    generate_me_menu('settings');
+?>
+</div>
+<div class="col-sm-9">
+<h2>Settings</h2>
 <h3>Under construction</h3>
+</div>
