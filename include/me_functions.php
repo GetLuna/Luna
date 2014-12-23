6552d4002fe09fe16d7a53be7d0d9f799e89237b
Yannick <contact@studio384.be>
2014-12-23 10:47:01 +0100

    #1028 Remove old Settings and Personality pages

--- include/me_functions.php
+++ include/me_functions.php
@@ -18,10 +18,7 @@ function load_me_nav($page = '') {
 	<?php endif; ?>
 </div>
 <div class="list-group">
-    <?php if ($luna_user['id'] == $id && !$luna_user['is_guest'] || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1'))): ?>
-        <a class="<?php if ($page == 'personality') echo 'active'; ?> list-group-item" href="me.php?section=personality&amp;id=<?php echo $id ?>"><?php echo $lang['Section personality'] ?></a>
-        <a class="<?php if ($page == 'settings') echo 'active'; ?> list-group-item" href="me.php?section=settings&amp;id=<?php echo $id ?>"><?php echo $lang['Settings'] ?></a>
-    <?php endif; if ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1')): ?>
+    <?php if ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1')): ?>
 		<a class="<?php if ($page == 'admin') echo 'active'; ?> list-group-item" href="me.php?section=admin&amp;id=<?php echo $id ?>"><?php echo $lang['Section admin'] ?></a>
 	<?php endif; ?>
 </div>
