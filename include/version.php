06d905531baa84fc22377180c48b3ac1d6577b62
Yannick <contact@studio384.be>
2014-12-27 00:11:09 +0100

    #1034 Update images

--- include/version.php
+++ include/version.php
@@ -11,7 +11,7 @@ class Version {
 	const FORUM_VERSION = '0.2-dev.2015';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '0.2.3551';
+	const FORUM_CORE_VERSION = '0.2.3555';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = '87.10';
