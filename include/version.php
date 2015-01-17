d8551172a1d219fe7dbfb38d58f973d2ca8b1933
Yannick <contact@studio384.be>
2015-01-17 22:17:27 +0100

    #1005 Change build 3640 to 3652 - udate about

--- include/version.php
+++ include/version.php
@@ -10,7 +10,7 @@ class Version {
 	const FORUM_VERSION = '0.2.0';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '0.2.3640';
+	const FORUM_CORE_VERSION = '0.2.3652';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = '87.14';
