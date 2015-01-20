cdf73669aea89ec4882acc7a004199a6bc6ca124
Yannick <contact@studio384.be>
2015-01-20 19:33:07 +0100

    #900 Change color of post button

--- viewforum.php
+++ viewforum.php
@@ -51,7 +51,7 @@ switch ($cur_forum['sort_by']) {
 
 // Can we or can we not post new topics?
 if (($cur_forum['post_topics'] == '' && $luna_user['g_post_topics'] == '1') || $cur_forum['post_topics'] == '1' || $is_admmod)
-	$post_link = "\t\t\t".'<a class="btn btn-primary btn-post pull-right" href="post.php?fid='.$id.'">'.$lang['Post new topic'].'</a>'."\n";
+	$post_link = "\t\t\t".'<a class="btn btn-default btn-post pull-right" href="post.php?fid='.$id.'">'.$lang['Post new topic'].'</a>'."\n";
 else
 	$post_link = '';
 
