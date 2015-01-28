cba31b876476070eab1e69a1e8b046ccc3d312af
Yannick <contact@studio384.be>
2015-01-28 21:01:28 +0100

    #1075 More improvements to Sunset

--- style/Sunset/index.php
+++ style/Sunset/index.php
@@ -66,7 +66,7 @@ if ($luna_user['first_run'] == '0') {
 	</div>
 	<div class="col-sm-9 hidden-xs">
 		<div class="list-group list-group-forum">
-			<?php draw_forum_list('viewforum.php', $id) ?>
+			<?php draw_forum_list('viewforum.php', 'forum.php') ?>
 		</div>
 	</div>
 </div>
