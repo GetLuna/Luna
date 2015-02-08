bf5ae4577c5052a45d45bff035f37d758dc09615
Yannick <contact@studio384.be>
2015-02-08 11:18:24 +0100

    #993 Tag ModernBB 3.7

--- include/version.php
+++ include/version.php
@@ -9,10 +9,10 @@
 class Version
 {
 	// See http://modernbb.be/docs/version.php for more info
-	const FORUM_VERSION = '3.7-dev';
+	const FORUM_VERSION = '3.7.0';
 
 	// The ModernBB Core version
-	const FORUM_CORE_VERSION = '0.0.37.2591';
+	const FORUM_CORE_VERSION = '0.0.37.2592';
 
 	// The database version number, every change in the database requires this number to go one up
 	const FORUM_DB_VERSION = 77;
