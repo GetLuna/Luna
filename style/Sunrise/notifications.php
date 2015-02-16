f6e3bcbbbcc9cb26ea12854b381087d5c5673cf5
Yannick <contact@studio384.be>
2015-02-16 10:35:42 +0100

    #905 Remove logic for notifications from theme

--- style/Sunrise/notifications.php
+++ style/Sunrise/notifications.php
@@ -3,36 +3,6 @@
 // Make sure no one attempts to run this view directly.
 if (!defined('FORUM'))
 	exit;
-
-// Show notifications
-$result = $db->query('SELECT COUNT(id) FROM '.$db_prefix.'notifications WHERE viewed = 0 AND user_id = '.$id) or error ('Unable to load notifications', __FILE__, __LINE__, $db->error());
-$num_not_unseen = $db->result($result);
-
-if ($num_not_unseen == '0') {
-	$ind_not[] = '<a class="list-group-item disabled" href="notifications.php?id='.$id.'">No new notifications</a>';
-} else {
-	$result = $db->query('SELECT * FROM '.$db_prefix.'notifications WHERE viewed = 0 AND user_id = '.$id.' ORDER BY time DESC') or error ('Unable to load notifications', __FILE__, __LINE__, $db->error());
-	while ($cur_notifi = $db->fetch_assoc($result)) {
-		$notifitime = format_time($cur_notifi['time'], false, null, $time_format, true, true);
-		$ind_not[] = '<a class="list-group-item" href="'.$cur_notifi['link'].'"><span class="fa fa-fw '.$cur_notifi['icon'].'"></span> '.$cur_notifi['message'].' <span class="timestamp pull-right">'.$notifitime.'</span></a>';
-	}
-}
-
-$result = $db->query('SELECT COUNT(id) FROM '.$db_prefix.'notifications WHERE viewed = 1 AND user_id = '.$id) or error ('Unable to load notifications', __FILE__, __LINE__, $db->error());
-$num_not_seen = $db->result($result);
-
-if ($num_not_seen == '0') {
-	$ind_not_seen[] = '<a class="list-group-item disabled" href="notifications.php?id='.$id.'">No new notifications</a>';
-} else {
-	$result = $db->query('SELECT * FROM '.$db_prefix.'notifications WHERE viewed = 1 AND user_id = '.$id.' ORDER BY time DESC') or error ('Unable to load notifications', __FILE__, __LINE__, $db->error());
-	while ($cur_notifi = $db->fetch_assoc($result)) {
-		$notifitime = format_time($cur_notifi['time'], false, null, $time_format, true, true);
-		$ind_not_seen[] = '<a class="list-group-item" href="'.$cur_notifi['link'].'"><span class="fa fa-fw '.$cur_notifi['icon'].'"></span> '.$cur_notifi['message'].' <span class="timestamp pull-right">'.$notifitime.'</span></a>';
-	}
-}
-
-$not = implode('', $ind_not);
-$not_seen = implode('', $ind_not_seen);
 ?>
 <div class="col-sm-3 profile-nav">
 	<div class="user-card-profile">
