484398e49615753b3ab6d8dbc25e6f2c1ca18c23
Yannick <contact@studio384.be>
2015-03-05 22:17:51 +0100

    #1081 Fix issues with Backstage icons

--- backstage/header.php
+++ backstage/header.php
@@ -22,6 +22,7 @@ $p = isset($p) ? $p : null;
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="ROBOTS" content="NOINDEX, FOLLOW" />
