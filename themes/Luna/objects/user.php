462e4edfbc7a8b0e524021ff38311d78f800e5b3
Yannick <contact@studio384.be>
2015-03-13 17:24:34 +0100

    #1072 Fix for long names in the userlist

--- themes/Luna/objects/user.php
+++ themes/Luna/objects/user.php
@@ -1,3 +1,9 @@
+<?php
+if (luna_strlen(luna_htmlspecialchars($user_data['username'])) > 14)
+	$cur_user_name = utf8_substr(luna_htmlspecialchars($user_data['username']), 0, 12).'...';
+else
+	$cur_user_name = luna_htmlspecialchars($user_data['username']);
+?>
 <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
 	<div class="user-entry">
 		<div class="media">
@@ -5,7 +11,7 @@
 				<?php echo $user_avatar; ?>
 			</a>
 			<div class="media-body">
-				<h2 class="media-heading"><?php echo '<a href="profile.php?id='.$user_data['id'].'">'.luna_htmlspecialchars($user_data['username']).'</a>' ?></h2>
+				<h2 class="media-heading"><?php echo '<a href="profile.php?id='.$user_data['id'].'">'.$cur_user_name.'</a>' ?></h2>
 				<h4><?php echo $user_title_field ?></h4>
 				<?php echo forum_number_format($user_data['num_posts']) ?> posts since <?php echo format_time($user_data['registered'], true) ?>
 			</div>
