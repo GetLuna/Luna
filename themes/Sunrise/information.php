2a41c26311b7ee6d66b5140e3e4050a9b84d1e4e
Yannick <contact@studio384.be>
2015-03-17 20:05:06 +0100

    #1107 Fix unreadable no users message

--- themes/Sunrise/information.php
+++ themes/Sunrise/information.php
@@ -15,10 +15,10 @@ $xmlstr = <<<XML
 	<id>sunrise</id>
 	<name>Sunrise</name>
 	<developer>Studio 384</developer>
-	<date>12 March 2015</date>
-	<version>0.4.1</version>
-	<minversion>1.0 Preview 4 Update 1</minversion>
-	<maxversion>1.0 Preview 4 Update 1</maxversion>
+	<date>17 March 2015</date>
+	<version>0.4.2</version>
+	<minversion>1.0 Preview 4 Update 2</minversion>
+	<maxversion>1.0 Preview 4 Update 2</maxversion>
 	<description>Sunset is a variant upon Luna, featuring a more classic design.</description>
 	<parent_theme>Luna</parent_theme>
 </style>
