64a0021eaf62debc771e25389d72c698f3b00921
Yannick <contact@studio384.be>
2015-03-19 22:03:56 +0100

    #1071 Update comment design further

--- themes/Random/thread.php
+++ themes/Random/thread.php
@@ -44,11 +44,11 @@ if (!defined('FORUM'))
 	</div>
 	<div class="col-sm-9">
 		<h2><?php echo luna_htmlspecialchars($cur_topic['subject']) ?></h2>
-		<span class="pull-right"><?php echo $paging_links ?></span>
+		<?php echo $paging_links ?>
 		<?php draw_topic_list(); ?>
 		<form method="post" action="post.php?tid=<?php echo $id ?>" onsubmit="this.submit.disabled=true;if(process_form(this)){return true;}else{this.submit.disabled=false;return false;}">
 		<?php draw_editor('10'); ?>
 		</form>
-		<div class="pull-right"><?php echo $paging_links ?></div>
+		<?php echo $paging_links ?>
 	</div>
 </div>
