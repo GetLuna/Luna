c5c7adb248caae21c927f1782b5da3572d54d7c5
Yannick <contact@studio384.be>
2015-03-22 10:10:13 +0100

    #1071 Only show sidebar when subforums are available

--- themes/Random/forum.php
+++ themes/Random/forum.php
@@ -17,29 +17,29 @@ $jumbo_style = 'style="background:'.$cur_forum['color'].';"';
 	<div class="row">
 		<div class="col-xs-12">
 			<div class="btn-group">
-			<a class="btn btn-primary" href="index.php"><span class="fa fa-fw fa-home"></span></a>
-			<a class="btn btn-primary" href="viewforum.php?id=<?php echo $forum_id ?>"><?php echo luna_htmlspecialchars($cur_forum['forum_name']) ?></a>
+				<a class="btn btn-primary" href="index.php"><span class="fa fa-fw fa-home"></span></a>
+				<a class="btn btn-primary" href="viewforum.php?id=<?php echo $forum_id ?>"><?php echo luna_htmlspecialchars($cur_forum['forum_name']) ?></a>
 			</div>
 			<span class="pull-right"><?php echo $post_link ?><?php echo $paging_links ?></span>
 		</div>
 	</div>
 	<div class="row forumview">
+		<?php if ((is_subforum($id) && $id != '0')) { ?>
 		<div class="col-sm-3">
-			<?php if ((is_subforum($id) && $id != '0')): ?>
 				<h5 class="list-group-head">Subforums</h5>
 				<div class="list-group list-group-forum">
 					<?php draw_subforum_list('viewforum.php', 'subforum.php') ?>
 				</div>
-				<hr />
-			<?php endif; ?>
-			<div class="list-group list-group-forum">
-				<?php draw_mark_read('list-group-item', 'forumview') ?>
-				<?php if ($id != '0' && $is_admmod) { ?>
-					<a class="list-group-item" href="backstage/moderate.php?fid=<?php echo $forum_id ?>&p=<?php echo $p ?>"><span class="fa fa-fw fa-eye"></span> <?php echo $lang['Moderate forum'] ?></a>
-				<?php } ?>
-			</div>
 		</div>
 		<div class="col-sm-9">
+			<?php } else { ?>
+		<div class="col-xs-12">
+			<?php } ?>
 			<?php draw_topics_list(); ?>
+			<div class="btn-group">
+				<?php if ($id != '0' && $is_admmod) { ?>
+					<a class="btn btn-primary" href="backstage/moderate.php?fid=<?php echo $forum_id ?>&p=<?php echo $p ?>"><span class="fa fa-fw fa-eye"></span> <?php echo $lang['Moderate forum'] ?></a>
+				<?php } ?>
+			</div>
 		</div>
 	</div>
