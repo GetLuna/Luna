9a5d98e91e14f904cc447fa7c48a3586d7a9f312
Yannick <contact@studio384.be>
2015-03-29 10:24:22 +0200

    Update theme information, new screenshots

--- themes/Luna/information.php
+++ themes/Luna/information.php
@@ -15,10 +15,10 @@ $xmlstr = <<<XML
 	<id>luna</id>
 	<name>Luna</name>
 	<developer>Studio 384</developer>
-	<date>25 March 2015</date>
-	<version>0.5.0</version>
-	<minversion>1.0 Preview 4 Update 2</minversion>
-	<maxversion>1.0 Beta 1</maxversion>
+	<date>29 March 2015</date>
+	<version>0.7.0</version>
+	<minversion>1.0 Beta 2</minversion>
+	<maxversion>1.0 Beta 3</maxversion>
 	<description>Luna is the default theme of the Luna software, this modern, fresh, yet colorfull design makes your board an unique piece!</description>
 </style>
 XML;
