75b7e17f5bbcc9a24021bb8f7f784f866a5fb8f8
Yannick <contact@studio384.be>
2015-04-05 20:55:17 +0200

    #1190 Add Open Sans inbetween

--- include/version.php
+++ include/version.php
@@ -10,7 +10,7 @@ class Version {
 	const FORUM_VERSION = '0.9-dev';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '0.9.4176';
+	const FORUM_CORE_VERSION = '0.9.4189';
 	
 	// The Luna Core code name
 	const FORUM_CODE_NAME = 'aero';
