584a302091dcbe322c4e124d727dd6b126f15803
Yannick <contact@studio384.be>
2015-04-06 22:48:59 +0200

    #1005 Update about as of #1207 fix

--- include/version.php
+++ include/version.php
@@ -10,7 +10,7 @@ class Version {
 	const FORUM_VERSION = '0.9-dev';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '0.9.4219';
+	const FORUM_CORE_VERSION = '0.9.4222';
 	
 	// The Luna Core code name
 	const FORUM_CODE_NAME = 'aero';
