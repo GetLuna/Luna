f67252d1f5065c5903016f041fecd0633b1de9d1
Yannick <contact@studio384.be>
2015-04-06 17:33:51 +0200

    Do not show Inbox when disabled

--- include/me_functions.php
+++ include/me_functions.php
@@ -12,16 +12,18 @@ function load_me_nav($page = '') {
 ?>
 <div class="list-group">
 	<a class="<?php if ($page == 'profile') echo 'active'; ?> list-group-item" href="profile.php?id=<?php echo $id ?>"><?php echo $lang['Profile'] ?></a>
-	<?php if ($luna_user['id'] == $id && !$luna_user['is_guest'] || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1')) || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1'))): ?>
 </div>
-<div class="list-group">
-		<a class="<?php if ($page == 'inbox') echo 'active'; ?> list-group-item" href="inbox.php"><?php echo $lang['Inbox'] ?></a>
+<?php if ($luna_user['id'] == $id && !$luna_user['is_guest'] || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1')) || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1'))): ?>
+	<div class="list-group">
+		<?php if ($luna_config['o_pms_enabled'] == '1' && $luna_user['g_pm'] == '1' && $luna_user['use_pm'] == '1'): ?>
+			<a class="<?php if ($page == 'inbox') echo 'active'; ?> list-group-item" href="inbox.php"><?php echo $lang['Inbox'] ?></a>
+		<?php endif; ?>
 		<a class="<?php if ($page == 'notifications') echo 'active'; ?> list-group-item" href="notifications.php"><?php echo $lang['Notifications'] ?></a>
-</div>
-<div class="list-group">
+	</div>
+	<div class="list-group">
 		<a class="<?php if ($page == 'settings') echo 'active'; ?> list-group-item" href="settings.php?id=<?php echo $id ?>"><?php echo $lang['Settings'] ?></a>
-	<?php endif; ?>
-</div>
+	</div>
 <?php
+endif;
 
 }
