8fe08baf7a941bdf5aae3131bc5a33cbebd16974
Charlie Merland <charlie@caercam.org>
2015-04-17 12:44:11 +0200

    Start implementing Backbone

--- themes/Luna/header.php
+++ themes/Luna/header.php
@@ -19,7 +19,7 @@ else
 		<link rel="stylesheet" type="text/css" href="include/css/prism.css" />
 		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-		<script src="include/js/prism.js"></script>
+		<script src="include/js/vendor/prism.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 // Allow childs
