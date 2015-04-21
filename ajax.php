60f45fbfedd4d949c96b20026201b1cc6eb66a84
Charlie Merland <charlie@caercam.org>
2015-04-22 01:52:30 +0200

    Coding convention

--- ajax.php
+++ ajax.php
@@ -5,26 +5,26 @@
  * License: http://opensource.org/licenses/MIT MIT
  */
 
-define( 'FORUM_ROOT', dirname( __FILE__ ) . '/' );
+define('FORUM_ROOT', dirname(__FILE__) . '/');
 require FORUM_ROOT.'include/common.php';
-define( 'DOING_AJAX', true );
+define('DOING_AJAX', true);
 
 // Load AJAX handlers
 require FORUM_ROOT . 'include/ajax_functions.php';
 require FORUM_ROOT . 'include/ajax_actions.php';
 
-$allowed_actions = array( 'heartbeat', 'fetch-notifications', 'read-notification', 'trash-notification' );
+$allowed_actions = array('heartbeat', 'fetch-notifications', 'read-notification', 'trash-notification');
 
 // Register core Ajax calls.
-if ( ! empty( $_GET['action'] ) && in_array( $_GET['action'], $allowed_actions ) )
-	$action = 'luna_ajax_' . str_replace( '-', '_', $_GET['action'] );
+if (!empty($_GET['action']) && in_array($_GET['action'], $allowed_actions))
+	$action = 'luna_ajax_' . str_replace('-', '_', $_GET['action']);
 
-if ( ! empty( $_POST['action'] ) && in_array( $_POST['action'], $allowed_actions ) )
-	$action = 'luna_ajax_' . str_replace( '-', '_', $_POST['action'] );
+if (!empty($_POST['action']) && in_array($_POST['action'], $allowed_actions))
+	$action = 'luna_ajax_' . str_replace('-', '_', $_POST['action']);
 
-if ( ! is_null( $action ) && function_exists( $action ) ) {
-	call_user_func( $action );
+if (!is_null($action) && function_exists($action)) {
+	call_user_func($action);
 }
 
 // Default status
-die( '0' );
+die('0');
