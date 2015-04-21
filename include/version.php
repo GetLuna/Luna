54362d09f5acf31fa4dc55e39f12613efb112015
Yannick <contact@studio384.be>
2015-04-20 21:01:39 -0700

    Update version to match with new commits

--- include/version.php
+++ include/version.php
@@ -10,7 +10,7 @@ class Version {
 	const FORUM_VERSION = '1.1-alpha';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '1.1.4381';
+	const FORUM_CORE_VERSION = '1.1.4422';
 	
 	// The Luna Core code name
 	const FORUM_CODE_NAME = 'bittersweetshimmer';
