a3eb536faa97f8faa5fc7b60e94d291a350a0783
Yannick <contact@studio384.be>
2015-05-17 13:52:53 +0200

    #727 Add the new update system

---
+++ backstage/update_launch.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Copyright (C) 2013-2015 Luna
+ * Based on code by FluxBB copyright (C) 2008-2012 FluxBB
+ * Based on code by Rickard Andersson copyright (C) 2002-2008 PunBB
+ * Licensed under GPLv3 (http://getluna.org/license.php)
+ */
+
+define('FORUM_ROOT', '../');
+require FORUM_ROOT.'include/common.php';
+
+if (!$luna_user['is_admmod'])
+	header("Location: login.php");
+
+require('update_class.php');
+
+$update = new AutoUpdate(true);
+$update->currentVersion = 0; //Must be an integer - you can't compare strings
+$update->updateUrl = 'https://raw.githubusercontent.com/GetLuna/UpdateService/master'; //Replace with your server update directory
+
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+
+if (file_exists(FORUM_CACHE_DIR.'cache_update.php'))
+	include FORUM_CACHE_DIR.'cache_update.php';
+	
+if ((!defined('FORUM_UPDATE_LOADED') || ($last_check_time > time() + (60 * 60 * 24)))) {
+	if (!defined('FORUM_CACHE_FUNCTIONS_LOADED'))
+		require FORUM_ROOT.'include/cache.php';
+
+	generate_update_cache();
+	require FORUM_CACHE_DIR.'cache_update.php';
+}
+
+$page_title = array(luna_htmlspecialchars($luna_config['o_board_title']), __('Admin', 'luna'), __('Update', 'luna'));
+define('FORUM_ACTIVE_PAGE', 'admin');
+require 'header.php';
+	load_admin_nav('backstage', 'update');
+?>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Luna one-click update</h3>
+			</div>
+			<div class="panel-body">
+<?php
+$latest = $update->checkUpdate();
+if ($latest !== false) {
+	if ($latest > $update->currentVersion) {
+		//Install new update
+		echo "New version: ".$update->latestVersionName."<br>";
+		echo "Installing Luna...<br>";
+		if ($update->update()) {
+			echo "Update successful!";
+		}
+		else {
+			echo "Update failed!";
+		}
+		
+	}
+	else {
+		echo "Current version is up to date";
+	}
+}
+else {
+	echo $update->getLastError();
+}
+?>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+
+require 'footer.php';
