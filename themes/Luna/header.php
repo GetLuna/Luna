359f6d7888e76a1ab0a5f5dcf9b7924d7a1b2f01
Yannick <contact@studio384.be>
2015-05-22 21:21:14 +0200

    Make sure Lunicons can be displayed

--- themes/Luna/header.php
+++ themes/Luna/header.php
@@ -17,6 +17,7 @@ else
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="include/css/prism.css" />
+		<link rel="stylesheet" type="text/css" href="include/css/lunicons.css" />
 		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		<script src="include/js/vendor/prism.js"></script>
