3dea7870a022ae2c7f5fc4f90ef01c8f30acc1bf
Yannick <contact@studio384.be>
2015-05-31 17:36:46 +0200

    Fix wrong order

--- themes/Luna/header.php
+++ themes/Luna/header.php
@@ -6,13 +6,13 @@ check_night_mode();
 <!DOCTYPE html>
 <html class="<?php echo $body_classes ?>">
 	<head>
-		<?php load_meta(); ?>
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="include/css/prism.css" />
 		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		<script src="include/js/vendor/prism.js"></script>
+		<?php load_meta(); ?>
 		<style>
 		.emoji {
 			font-size: <?php echo $luna_config['o_emoji_size'] ?>px;
