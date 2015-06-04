59254c60a9ca0ee7d8fac588bb12c2f5ac7466ca
Yannick <contact@studio384.be>
2015-06-04 16:44:20 +0200

    Visual improvements to first run

--- themes/Luna/index.php
+++ themes/Luna/index.php
@@ -9,7 +9,7 @@ if ($luna_user['first_run'] == '0') {
 <div class="first-run panel panel-default">
 	<div class="row first-run-content">
 		<div class="col-md-4 col-sm-6 col-xs-5 first-run-profile">
-			<h3 class="first-run-title"><?php echo sprintf(__('Hi there, %s', 'luna'), luna_htmlspecialchars($luna_user['username'])) ?></h3>
+			<h3 class="first-run-title"><span class="hidden-xs"><?php echo sprintf(__('Hi there, %s', 'luna'), luna_htmlspecialchars($luna_user['username'])) ?></span><span class="visible-xs-block"><?php echo luna_htmlspecialchars($luna_user['username']) ?></span></h3>
 			<span class="first-run-avatar thumbnail">
 				<?php echo $user_avatar ?>
 			</span>
