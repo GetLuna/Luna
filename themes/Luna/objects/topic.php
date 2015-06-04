0e582e2c8b544fae26fa56207c74215688ae6266
Yannick <contact@studio384.be>
2015-06-04 16:46:04 +0200

    Show forum name in small screen too

--- themes/Luna/objects/topic.php
+++ themes/Luna/objects/topic.php
@@ -7,9 +7,7 @@
 	<?php if ($cur_topic['moved_to'] == 0) { ?>
 		<span class="text-muted"> &middot; 
 			<?php echo $last_post_date ?>
-			&middot; <span class="hidden-xs">
-				<?php echo $forum_name ?>
-			</span>
+			&middot; <?php echo $forum_name ?>
 			<?php if ($cur_topic['moved_to'] == 0) { ?><span class="label label-default"><?php echo forum_number_format($cur_topic['num_replies']) ?></span><?php } ?>
 		</span>
 	<?php } ?>
