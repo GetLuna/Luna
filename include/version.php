ef7e0806952beb2d131915cf7aa2927fe06e7a94
Yannick <contact@studio384.be>
2015-06-15 13:49:41 +0200

    Update version information

--- include/version.php
+++ include/version.php
@@ -7,21 +7,21 @@
 
 class Version {
 	// See http://getluna.org/docs/version.php for more info
-	const FORUM_VERSION = '1.1-beta';
-	const FORUM_CORE_VERSION = '1.1.4719';
+	const FORUM_VERSION = '0.0 Preview';
+	const FORUM_CORE_VERSION = '1.1.4725';
 	const LUNA_CORE_REVISION = 99;
 
 	// The Luna Core code name
-	const FORUM_CODE_NAME = 'bittersweetshimmer';
+	const FORUM_CODE_NAME = 'bittersweetshimmer_mini';
 
 	// The database version number, every change in the database requires this number to go one up
-	const FORUM_DB_VERSION = '88.10';
+	const FORUM_DB_VERSION = '0.00';
 
 	// The parser version number, every change to the parser requires this number to go one up
-	const FORUM_PARSER_VERSION = '11.1.3';
+	const FORUM_PARSER_VERSION = '0.00';
 
 	// The search index version number, every change to the search index requires this number to go one up
-	const FORUM_SI_VERSION = '2.0';
+	const FORUM_SI_VERSION = '0.00';
 
 	// Luna system requirements
 	const MIN_PHP_VERSION = '5.2.0';
