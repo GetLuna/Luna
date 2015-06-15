4b2f60660d4b8dce3c2c1030dff0798dde0357b3
Yannick <contact@studio384.be>
2015-06-15 13:46:56 +0200

    Drop Sunrise

--- themes/Sunrise/objects/subforum.php
+++
@@ -1,8 +0,0 @@
-<?php
-$style = '';
-
-if ($current_id == $cur_forum['fid'])
-?>
-<a href="<?php echo $page ?>?id=<?php echo $cur_forum['fid'] ?>" class="list-group-item <?php echo $item_status ?>">
-	<?php echo luna_htmlspecialchars($cur_forum['forum_name']) ?>
-</a>
