9ef0abe0894efa96bd97095bb3b2df1a7380b7d3
Yannick <contact@studio384.be>
2015-06-15 20:56:25 +0200

    Revert "Drop Sunrise"
    This reverts commit 4b2f60660d4b8dce3c2c1030dff0798dde0357b3.

---
+++ themes/Sunrise/information.php
@@ -0,0 +1,26 @@
+<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE extension SYSTEM "ext-1.0.dtd">
+
+<!--
+
+/*
+ * Copyright (C) 2014-2015 Studio 384
+ * Licensed under GPLv3 (http://getluna.org/license.php)
+ */
+-->
+
+<style engine="1.0">
+	<id>sunrise</id>
+	<name>Sunrise</name>
+	<developer>Studio 384</developer>
+	<date>12 June 2015</date>
+	<version>1.1 Preview</version>
+	<minversion>1.1 Beta</minversion>
+	<maxversion>1.1 Beta</maxversion>
+	<description>Sunset is a variant upon Fifteen, featuring a more classic design.</description>
+	<parent_theme>Fifteen</parent_theme>
+</style>
+XML;
+?>
---
+++ themes/Sunrise/objects/category.php
@@ -0,0 +1,5 @@
+<div id="idx<?php echo $cat_count ?>">
+	<div class="category-box">
+        <div class="row alert alert-info alert-section alert-all">
+            <h3><?php echo luna_htmlspecialchars($cur_forum['cat_name']) ?></h3>
+        </div>
