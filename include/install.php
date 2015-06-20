853727b6b3bf42b2c96da8b7bd14b34a942647e2
Yannick <contact@studio384.be>
2015-06-20 11:16:24 -0700

    #1305 SQLite 3 support

--- include/install.php
+++ include/install.php
@@ -49,6 +49,9 @@ class Installer {
 		if (function_exists('sqlite_open'))
 			$db_extensions[] = array('sqlite', 'SQLite');
 
+		if (class_exists('SQLite3'))
+			$db_extensions[] = array('sqlite3', 'SQLite3');
+
 		if (function_exists('pg_connect'))
 			$db_extensions[] = array('pgsql', 'PostgreSQL');
 			
@@ -137,6 +140,10 @@ class Installer {
 			case 'sqlite':
 				require FORUM_ROOT.'include/dblayer/sqlite.php';
 				break;
+
+			case 'sqlite3':
+				require FORUM_ROOT.'include/dblayer/sqlite3.php';
+				break;
 	
 			default:
 				error(sprintf($lang['DB type not valid'], luna_htmlspecialchars($db_type)));
@@ -164,6 +171,7 @@ class Installer {
 				break;
 	
 			case 'sqlite':
+			case 'sqlite3':
 				if (strtolower($db_prefix) == 'sqlite_')
 					error($lang['Prefix reserved']);
 				break;
@@ -921,7 +929,7 @@ class Installer {
 			)
 		);
 	
-		if ($db_type == 'sqlite') {
+		if ($db_type == 'sqlite' || $db_type == 'sqlite3') {
 			$schema['PRIMARY KEY'] = array('id');
 			$schema['UNIQUE KEYS'] = array('word_idx'	=> array('word'));
 		}
