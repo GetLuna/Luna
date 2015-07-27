5c2168fb62a329305db62704f3836945c9e0480e
Yannick <contact@studio384.be>
2015-07-28 00:06:10 +0200

    #1338 Get all components in sync

--- themes/Luna/header.php
+++ themes/Luna/header.php
@@ -9,7 +9,7 @@ require ('header.php');
 		<title><?php echo generate_page_title($page_title, $p) ?></title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="include/css/prism.css" />
 		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
