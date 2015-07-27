9f3ec09d897c04f57bca21261423eac0bf845eb3
Yannick <contact@studio384.be>
2015-07-27 15:42:00 +0200

    #1329 Include jQuery 2.1.4 in Luna

--- backstage/footer.php
+++ backstage/footer.php
@@ -36,7 +36,7 @@ $db->end_transaction();
 ?>
 			</div>
 		</div>
-		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script src="../include/js/vendor/jquery.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	</body>
 </html>
