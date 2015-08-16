3f8238560cc4af6a63c622f9b591d741e1e99bd8
Yannick <contact@studio384.be>
2015-08-16 10:42:40 +0200

    Bump version

--- include/version.php
+++ include/version.php
@@ -10,7 +10,7 @@ class Version {
 	const FORUM_VERSION = '1.0.8-dev';
 
 	// The Luna Core version
-	const FORUM_CORE_VERSION = '1.0.4354';
+	const FORUM_CORE_VERSION = '1.0.4357';
 	
 	// The Luna Core code name
 	const FORUM_CODE_NAME = 'aero';
