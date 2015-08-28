525b0c10de0e8f6aeba138bf75050bc80448fd34
Yannick <contact@studio384.be>
2015-08-28 13:14:19 +0200

    #1334 Start rewriting the profile interface

--- themes/Sixteen/profile.php
+++ themes/Sixteen/profile.php
@@ -5,56 +5,59 @@ if (!defined('FORUM'))
 	exit;
 
 ?>
-<div class="col-sm-3 profile-nav">
-	<div class="user-card-profile">
-		<h3 class="user-card-title"><?php echo luna_htmlspecialchars($user['username']) ?></h3>
-		<span class="user-card-avatar thumbnail">
-			<?php echo $avatar_user_card ?>
-		</span>
-	</div>
-<?php
-	load_me_nav('profile');
-?>
-</div>
-<div class="profile col-sm-9">
-	<nav class="navbar navbar-default" role="navigation">
-		<div class="navbar-header">
-			<a href="profile.php?id=<?php echo $id ?>" class="navbar-brand"><span class="fa fa-fw fa-user"></span> <?php echo luna_htmlspecialchars($user['username']) ?></a>
-		</div>
-	</nav>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title"><?php _e('About user', 'luna') ?></h3>
-		</div>
-		<div class="panel-body">
-			<?php echo implode("\n\t\t\t\t\t\t\t".'<br />', $user_personality)."\n" ?>
+<div class="row">
+	<div class="col-sm-3 profile-nav">
+		<div class="profile-card">
+			<div class="profile-card-head">
+				<div class="user-avatar thumbnail">
+					<?php echo $avatar_user_card; ?>
+				</div>
+				<h2><?php echo $user_username; ?></h2>
+				<h3><?php echo $user_usertitle; ?></h3>
+			</div>
+			<?php load_me_nav('profile'); ?>
 		</div>
 	</div>
-<?php if (!empty($user_messaging)): ?>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title"><?php _e('Contact', 'luna'); ?></h3>
+	<div class="profile col-sm-9">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="navbar-header">
+				<a href="profile.php?id=<?php echo $id ?>" class="navbar-brand"><span class="fa fa-fw fa-user"></span> <?php echo luna_htmlspecialchars($user['username']) ?></a>
+			</div>
+		</nav>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php _e('About user', 'luna') ?></h3>
+			</div>
+			<div class="panel-body">
+				<?php echo implode("\n\t\t\t\t\t\t\t".'<br />', $user_personality)."\n" ?>
+			</div>
 		</div>
-		<div class="panel-body">
-			<p><?php echo implode("\n\t\t\t\t\t\t\t".'<br />', $user_messaging)."\n" ?></p>
+	<?php if (!empty($user_messaging)): ?>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php _e('Contact', 'luna'); ?></h3>
+			</div>
+			<div class="panel-body">
+				<p><?php echo implode("\n\t\t\t\t\t\t\t".'<br />', $user_messaging)."\n" ?></p>
+			</div>
 		</div>
-	</div>
-<?php
-endif;
-
-if ($luna_config['o_signatures'] == '1') {
-	if (isset($parsed_signature)) {
-?>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title"><?php _e('Signature', 'luna'); ?></h3>
+	<?php
+	endif;
+	
+	if ($luna_config['o_signatures'] == '1') {
+		if (isset($parsed_signature)) {
+	?>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php _e('Signature', 'luna'); ?></h3>
+			</div>
+			<div class="panel-body">
+				<?php echo $user_signature ?>
+			</div>
 		</div>
-		<div class="panel-body">
-			<?php echo $user_signature ?>
-		</div>
-	</div>
-<?php
+	<?php
+		}
 	}
-}
-?>
+	?>
+	</div>
 </div>
