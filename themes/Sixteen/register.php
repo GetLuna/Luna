2e7761cd69514cf1041212292866811ec7964d07
Yannick <contact@studio384.be>
2015-09-07 00:02:05 +0200

    #1334 Update register

--- themes/Sixteen/register.php
+++ themes/Sixteen/register.php
@@ -5,10 +5,6 @@ if (!defined('FORUM'))
 	exit;
 
 ?>
-<nav class="navbar navbar-default" role="navigation">
-	<div class="navbar-header">
-		<a href="register.php" class="navbar-brand"><span class="fa fa-fw fa-user"></span> <?php _e('Register', 'luna') ?></a>
-	</div>
-</nav>
+<h2 class="profile-title"><?php _e('Register', 'luna') ?></h2>
 <?php draw_error_panel($errors); ?>
 <?php draw_registration_form(); ?>
