db5710d5dfd12c6aa3c0e30d77d91974e1183a9c
Yannick <contact@studio384.be>
2015-09-09 11:13:20 +0200

    Add new options to load_me_nav()

--- include/me_functions.php
+++ include/me_functions.php
@@ -6,23 +6,23 @@
  */
 
 // Display the me navigation
-function load_me_nav($page = '') {
+function load_me_nav($page = '', $list_group_class = '', $list_item_class = '') {
 	global $luna_config, $luna_user, $id;
 
 ?>
 <div class="hidden-xs">
-	<div class="list-group">
-		<a class="<?php if ($page == 'profile') echo 'active'; ?> list-group-item" href="profile.php?id=<?php echo $id ?>"><?php _e('Profile', 'luna') ?></a>
+	<div class="list-group <?php echo $list_group_class; ?>">
+		<a class="<?php if ($page == 'profile') echo 'active'; ?> list-group-item <?php echo $list_item_class; ?>" href="profile.php?id=<?php echo $id ?>"><?php _e('Profile', 'luna') ?></a>
 	</div>
 	<?php if ($luna_user['id'] == $id && !$luna_user['is_guest'] || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1')) || ($luna_user['g_id'] == FORUM_ADMIN || ($luna_user['g_moderator'] == '1' && $luna_user['g_mod_ban_users'] == '1'))): ?>
-		<div class="list-group">
+		<div class="list-group <?php echo $list_group_class; ?>">
 			<?php if ($luna_config['o_pms_enabled'] == '1' && $luna_user['g_pm'] == '1' && $luna_user['use_pm'] == '1'): ?>
-				<a class="<?php if ($page == 'inbox') echo 'active'; ?> list-group-item" href="inbox.php"><?php _e('Inbox', 'luna') ?></a>
+				<a class="<?php if ($page == 'inbox') echo 'active'; ?> list-group-item <?php echo $list_item_class; ?>" href="inbox.php"><?php _e('Inbox', 'luna') ?></a>
 			<?php endif; ?>
-			<a class="<?php if ($page == 'notifications') echo 'active'; ?> list-group-item" href="notifications.php"><?php _e('Notifications', 'luna') ?></a>
+			<a class="<?php if ($page == 'notifications') echo 'active'; ?> list-group-item <?php echo $list_item_class; ?>" href="notifications.php"><?php _e('Notifications', 'luna') ?></a>
 		</div>
-		<div class="list-group">
-			<a class="<?php if ($page == 'settings') echo 'active'; ?> list-group-item" href="settings.php?id=<?php echo $id ?>"><?php _e('Settings', 'luna') ?></a>
+		<div class="list-group <?php echo $list_group_class; ?>">
+			<a class="<?php if ($page == 'settings') echo 'active'; ?> list-group-item <?php echo $list_item_class; ?>" href="settings.php?id=<?php echo $id ?>"><?php _e('Settings', 'luna') ?></a>
 		</div>
 <?php endif; ?>
 </div>
