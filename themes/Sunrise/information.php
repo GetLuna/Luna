762fe5c1c181240d5c0744a998f94d09313e9074
Yannick <contact@studio384.be>
2015-10-09 16:39:04 +0200

    #1332 Tag Luna 1.2 Preview 3

--- themes/Sunrise/information.php
+++ themes/Sunrise/information.php
@@ -15,10 +15,10 @@ $xmlstr = <<<XML
 	<id>sunrise</id>
 	<name>Sunrise</name>
 	<developer>Studio 384</developer>
-	<date>13 September 2015</date>
+	<date>9 October 2015</date>
 	<version>1.2 Preview</version>
-	<minversion>1.2 Preview 1</minversion>
-	<maxversion>1.2 Preview 1</maxversion>
+	<minversion>1.2 Preview 3</minversion>
+	<maxversion>1.2 Preview 3</maxversion>
 	<description>Sunset is a variant upon Fifteen, featuring a more classic design.</description>
 	<parent_theme>Fifteen</parent_theme>
 </style>
