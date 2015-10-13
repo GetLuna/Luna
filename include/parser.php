7629e99eb06746d9044813c159154126341bac59
Yannick <contact@studio384.be>
2015-10-13 11:34:49 +0200

    #1390 Exclude newlines when parsing username in quote tag

--- include/parser.php
+++ include/parser.php
@@ -636,7 +636,7 @@ function do_bbcode($text, $is_signature = false) {
 
 	if (strpos($text, '[quote') !== false) {
 		$text = preg_replace('%\[quote\]\s*%', '</p><blockquote><p>', $text);
-		$text = preg_replace_callback('%\[quote=(&quot;|&\#039;|"|\'|)(.*?)\\1\]%s', create_function('$matches', 'return "<blockquote><footer><cite>".str_replace(array(\'[\', \'\\"\'), array(\'&#91;\', \'"\'), $matches[2])." ".__(\'wrote\',\'luna\')."</cite></footer><p>";'), $text);
+		$text = preg_replace_callback('%\[quote=(&quot;|&\#039;|"|\'|)([^\r\n]*?)\\1\]%s', create_function('$matches', 'return "<blockquote><footer><cite>".str_replace(array(\'[\', \'\\"\'), array(\'&#91;\', \'"\'), $matches[2])." ".__(\'wrote\',\'luna\')."</cite></footer><p>";'), $text);
 		$text = preg_replace('%\s*\[\/quote\]%S', '</p></blockquote><p>', $text);
 	}
 	if (!$is_signature) {
