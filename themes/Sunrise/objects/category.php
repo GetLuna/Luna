d7c6c44123b01aee5eea8788fee1ecf8790dd0f1
Yannick <contact@studio384.be>
2015-11-14 16:54:02 +0100

    Drop Sunrise

--- themes/Sunrise/objects/category.php
+++
@@ -1,5 +0,0 @@
-<div id="idx<?php echo $cat_count ?>">
-	<div class="category-box">
-        <div class="row alert alert-info alert-section alert-all">
-            <h3><?php echo luna_htmlspecialchars($cur_forum['cat_name']) ?></h3>
-        </div>
