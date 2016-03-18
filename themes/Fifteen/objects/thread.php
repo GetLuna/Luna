3c54b891f1db495eff46712d013b1e121d33936f
Yannick <contact@studio384.be>
2016-03-18 10:53:27 +0100

    Stop hiding "by" in mobile interface

--- themes/Fifteen/objects/thread.php
+++ themes/Fifteen/objects/thread.php
@@ -3,7 +3,7 @@
 	<span class="hidden-xs hidden-sm hidden-md hidden-lg">
 		<?php echo forum_number_format($thread_count + $start_from) ?>
 	</span>
-	<?php echo $subject_status ?> <a href="<?php echo $url ?>"><?php echo $subject ?></a><span class="hidden-xs"> <?php echo $by ?></span> <?php echo $subject_multipage ?>
+	<?php echo $subject_status ?> <a href="<?php echo $url ?>"><?php echo $subject ?></a> <?php echo $by ?> <?php echo $subject_multipage ?>
 	<?php if ($cur_thread['moved_to'] == 0) { ?>
 		<span class="text-muted"> &middot;
 			<?php echo $last_comment_date ?>
