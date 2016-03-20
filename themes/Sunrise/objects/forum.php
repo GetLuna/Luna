47c0145c1f6179bdc29869cd5c3250ce6b4caaa9
Yannick <contact@studio384.be>
2016-03-20 18:13:24 +0100

    Mark new items

--- themes/Sunrise/objects/forum.php
+++ themes/Sunrise/objects/forum.php
@@ -3,10 +3,10 @@
 		<a href="viewforum.php?id=<?php echo $cur_forum['fid'] ?>" style="color: <?php echo $cur_forum['color'] ?>" class="forum-title"><?php echo $faicon.luna_htmlspecialchars($cur_forum['forum_name']) ?></a><br />
 		<?php echo $forum_desc ?>
 	</div>
-	<div class="col-sm-1 hidden-xs text-center">
+	<div class="col-sm-1 hidden-xs">
 		<?php echo '<h5>'.$cur_forum['num_threads'].'</h5> <h6><small>'.$threads_label.'</small></h6>'; ?>
 	</div>
-	<div class="col-sm-1 hidden-xs text-center">
+	<div class="col-sm-1 hidden-xs">
 		<?php echo '<h5>'.$cur_forum['num_comments'].'</h5> <h6><small>'.$comments_label.'</small></h6>';  ?>
 	</div>
 	<div class="col-sm-5 col-xs-6 overflow">
