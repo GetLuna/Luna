922245996252c7e970b9da6bac1ce9744b594698
Yannick <contact@studio384.be>
2016-03-21 11:20:01 +0100

    More unification

--- themes/Sunrise/objects/thread.php
+++ themes/Sunrise/objects/thread.php
@@ -1,7 +1,7 @@
 <div class="list-group-item clearfix <?php echo $item_status ?><?php if ($cur_thread['soft'] == true) echo ' soft'; ?>">
 	<div class="col-md-6 col-sm-6 col-xs-6">
 		<?php echo $subject_status ?> <a href="<?php echo $url ?>" class="forum-title"><?php echo $subject ?></a><br />
-		<?php echo $by ?> <?php echo $subject_multipage ?>
+        <span class="thread-desc"><?php echo $by ?> <?php echo $subject_multipage ?></span>
 	</div>
 	<div class="col-md-1 col-sm-2 hidden-xs">
 		<?php echo '<h5>'.(($cur_thread['moved_to'] == 0)? $cur_thread['num_replies'] : '-').'</h5> <h6><small>'.$comments_label.'</small></h6>';  ?>
