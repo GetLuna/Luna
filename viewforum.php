662be804d498648dc133ed99117925d785622888
Yannick <contact@studio384.be>
2016-03-21 11:09:09 +0100

    Make it possible to mark a forum with unread items

--- viewforum.php
+++ viewforum.php
@@ -18,6 +18,36 @@ $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 if ($id < 1)
 	message(__('Bad request. The link you followed is incorrect, outdated or you are simply not allowed to hang around here.', 'luna'), false, '404 Not Found');
 
+
+
+// Get list of forums and threads with new comments since last visit
+if (!$luna_user['is_guest']) {
+	$result = $db->query('SELECT f.id, f.last_comment FROM '.$db->prefix.'forums AS f LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=f.id AND fp.group_id='.$luna_user['g_id'].') WHERE (fp.read_forum IS NULL OR fp.read_forum=1) AND f.last_comment>'.$luna_user['last_visit']) or error('Unable to fetch forum list', __FILE__, __LINE__, $db->error());
+
+	if ($db->num_rows($result)) {
+		$forums = $new_threads = array();
+		$tracked_threads = get_tracked_threads();
+
+		while ($cur_forum = $db->fetch_assoc($result)) {
+			if (!isset($tracked_threads['forums'][$cur_forum['id']]) || $tracked_threads['forums'][$cur_forum['id']] < $cur_forum['last_comment'])
+				$forums[$cur_forum['id']] = $cur_forum['last_comment'];
+		}
+
+		if (!empty($forums)) {
+			if (empty($tracked_threads['threads']))
+				$new_threads = $forums;
+			else {
+				$result = $db->query('SELECT forum_id, id, last_comment FROM '.$db->prefix.'threads WHERE forum_id IN('.implode(',', array_keys($forums)).') AND last_comment>'.$luna_user['last_visit'].' AND moved_to IS NULL') or error('Unable to fetch new threads', __FILE__, __LINE__, $db->error());
+
+				while ($cur_thread = $db->fetch_assoc($result)) {
+					if (!isset($new_threads[$cur_thread['forum_id']]) && (!isset($tracked_threads['forums'][$cur_thread['forum_id']]) || $tracked_threads['forums'][$cur_thread['forum_id']] < $forums[$cur_thread['forum_id']]) && (!isset($tracked_threads['threads'][$cur_thread['id']]) || $tracked_threads['threads'][$cur_thread['id']] < $cur_thread['last_comment']))
+						$new_threads[$cur_thread['forum_id']] = $forums[$cur_thread['forum_id']];
+				}
+			}
+		}
+	}
+}
+
 // Fetch some info about the forum
 if (!$luna_user['is_guest'])
 	$result = $db->query('SELECT f.forum_name, f.forum_desc, f.moderators, f.num_threads, f.sort_by, f.icon, f.color, f.solved, fp.create_threads, s.user_id AS is_subscribed FROM '.$db->prefix.'forums AS f LEFT JOIN '.$db->prefix.'forum_subscriptions AS s ON (f.id=s.forum_id AND s.user_id='.$luna_user['id'].') LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=f.id AND fp.group_id='.$luna_user['g_id'].') WHERE (fp.read_forum IS NULL OR fp.read_forum=1) AND f.id='.$id) or error('Unable to fetch forum info', __FILE__, __LINE__, $db->error());
