e7c5f85a4c737dd47e021a07b020377fe31746e7
Yannick <contact@studio384.be>
2016-03-22 14:25:29 +0100

    Update thread lists for mobile design

--- themes/Sunrise/objects/thread2.php
+++ themes/Sunrise/objects/thread2.php
@@ -2,7 +2,6 @@
 	<span class="hidden-xs hidden-sm hidden-md hidden-lg">
 		<?php echo forum_number_format($thread_count + $start_from) ?>
 	</span>
-	<a href="<?php echo $url ?>"><?php echo $subject ?></a>
-    <br />
-    <?php _e('In', 'luna' ) ?> <?php echo $forum_name ?> &middot; <span class="label label-default"><?php echo forum_number_format($cur_thread['num_replies']) ?></span>
+	<a href="<?php echo $url ?>"><?php echo $subject ?></a><span class="hidden-xs"><br /><?php _e('In', 'luna' ) ?></span><span class="hidden-md hidden-sm hidden-lg"> &middot; </span>
+    <?php echo $forum_name ?> &middot; <span class="label label-default"><?php echo forum_number_format($cur_thread['num_replies']) ?></span>
 </div>
