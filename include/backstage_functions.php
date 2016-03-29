db50b702e445d8f2720dc73a97994c82d6239f48
Yannick <contact@studio384.be>
2016-03-30 00:18:58 +0200

    #1544 Add the notification fly-out to the Backstage nav

--- include/backstage_functions.php
+++ include/backstage_functions.php
@@ -9,7 +9,48 @@
 // Display the admin navigation menu
 //
 function load_admin_nav($section, $page) {
-	global $luna_user, $luna_config, $is_admin;
+	global $luna_user, $luna_config, $is_admin, $db;
+
+// Check for new notifications
+$result = $db->query('SELECT COUNT(id) FROM '.$db->prefix.'notifications WHERE viewed = 0 AND user_id = '.$luna_user['id']) or error ('Unable to load notifications', __FILE__, __LINE__, $db->error());
+$num_notifications = $db->result($result);
+
+if ($luna_config['o_notification_flyout'] == 1) {
+	if ($num_notifications == '0') {
+		$notificon = '<span class="fa fa-fw fa-circle-o"></span>';
+		$ind_notification[] = '<li><a href="notifications.php">'.__( 'No new notifications', 'luna' ).'</a></li>';
+	} else {
+		$notificon = $num_notifications.' <span class="fa fa-fw fa-circle"></span>';
+		
+		$notification_result = $db->query('SELECT * FROM '.$db->prefix.'notifications WHERE user_id = '.$luna_user['id'].' AND viewed = 0 ORDER BY time DESC LIMIT 10') or error ('Unable to load notifications', __FILE__, __LINE__, $db->error());
+		while ($cur_notifi = $db->fetch_assoc($notification_result)) {
+			$notifitime = format_time($cur_notifi['time'], false, null, $luna_config['o_time_format'], true, true);
+			$ind_notification[] = '<li><a href="notifications.php?notification='.$cur_notifi['id'].'"><span class="fa fa-fw luni luni-fw '.$cur_notifi['icon'].'"></span> '.$cur_notifi['message'].' <span class="timestamp pull-right">'.$notifitime.'</span></a></li>';
+		}
+	}
+
+	$notifications = implode('<li class="divider"></li>', $ind_notification);
+	$notification_menu_item = '
+					<li class="dropdown">
+					<a href="#" class="dropdown-toggle'.(($num_notifications != 0)? ' flash' : '').'" data-toggle="dropdown">'.$notificon.'<span class="visible-xs-inline"> '.__( 'Notifications', 'luna' ).'</span></a>
+					<ul class="dropdown-menu notification-menu">
+						<li role="presentation" class="dropdown-header">'.__( 'Notifications', 'luna' ).'</li>
+						<li class="divider"></li>
+						'.$notifications.'
+						<li class="divider"></li>
+                        <li class="dropdown-footer hidden-xs"><a class="pull-right" href="../notifications.php">'.__('More', 'luna').' <i class="fa fa-fw fa-arrow-right"></i></a></li>
+                        <li class="dropdown-footer hidden-lg hidden-md hidden-sm"><a href="../notifications.php">'.__('More', 'luna').' <i class="fa fa-fw fa-arrow-right"></i></a></li>
+					</ul>
+				</li>';
+} else {
+	if ($num_notifications == '0')
+		$notificon = '<span class="fa fa-fw fa-circle-o"></span>';
+	else
+		$notificon = $num_notifications.' <span class="fa fa-fw fa-circle"></span>';
+
+	$notification_menu_item = '<li><a href="notifications.php" class="flash">'.$notificon.'<span class="visible-xs-inline"> '.__( 'Notifications', 'luna' ).'</span></a></li>';
+}
+    
 ?>
 <nav class="navbar navbar-default" role="navigation">
 	<div class="container">
@@ -59,6 +100,7 @@ function load_admin_nav($section, $page) {
 $logout_url = '../login.php?action=out&amp;id='.$luna_user['id'].'&amp;csrf_token='.luna_csrf_token();
 ?>
 			<ul class="nav navbar-nav navbar-right">
+                <?php echo $notification_menu_item ?>
 				<li class="dropdown usermenu">
 					<a href="../profile.php?id=<?php echo $luna_user['id'] ?>" class="dropdown-toggle dropdown-user" data-toggle="dropdown">
                         <?php echo draw_user_avatar($luna_user['id'], true, 'avatar'); ?><span class="hidden-lg hidden-md hidden-sm"> <?php echo luna_htmlspecialchars($luna_user['username']); ?></span>
@@ -66,8 +108,7 @@ $logout_url = '../login.php?action=out&amp;id='.$luna_user['id'].'&amp;csrf_toke
 					<ul class="dropdown-menu">
 						<li><a href="../profile.php?id=<?php echo $luna_user['id'] ?>"><i class="fa fa-fw fa-user"></i> <?php _e('Profile', 'luna') ?></a></li>
 						<li><a href="../inbox.php"><i class="fa fa-fw fa-paper-plane-o"></i> <?php _e('Inbox', 'luna') ?></a></li>
-						<li><a href="../notifications.php"><i class="fa fa-fw fa-circle-o"></i> <?php _e('Notifications', 'luna') ?></a></li>
-						<li><a href="../settings.php?id=<?php echo ''.$luna_user['id'] ?>"><i class="fa fa-fw fa-cogs"></i> <?php _e('Settings', 'luna') ?></a></li>
+						<li><a href="../settings.php?id=<?php echo $luna_user['id'] ?>"><i class="fa fa-fw fa-cogs"></i> <?php _e('Settings', 'luna') ?></a></li>
 						<li class="divider"></li>
 						<li><a href="../help.php"><i class="fa fa-fw fa-info-circle"></i> <?php _e('Help', 'luna') ?></a></li>
 						<li><a href="http://getluna.org"><i class="fa fa-fw fa-support"></i> <?php _e('Support', 'luna') ?></a></li>
