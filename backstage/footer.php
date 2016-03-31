47154e2266b995872e075ed376ae3c64a71512dc
Yannick <contact@studio384.be>
2016-03-31 18:06:05 +0200

    Add query table back

--- backstage/footer.php
+++ backstage/footer.php
@@ -11,24 +11,12 @@
 if (!defined('FORUM'))
 	exit;
 
-// Display debug info (if enabled/defined)
+
 if (defined('LUNA_DEBUG')) {
-	echo '<p id="debug">[ ';
-
-	// Calculate script generation time
-	$time_diff = sprintf('%.3f', get_microtime() - $luna_start);
-	echo sprintf(__('Generated in %1$s seconds, %2$s queries executed', 'luna'), $time_diff, $db->get_num_queries());
-
-	if (function_exists('memory_get_usage')) {
-		echo ' - '.sprintf(__('Memory usage: %1$s', 'luna'), file_size(memory_get_usage()));
-
-		if (function_exists('memory_get_peak_usage'))
-			echo ' '.sprintf(__('(Peak: %1$s)', 'luna'), file_size(memory_get_peak_usage()));
-	}
-
-	echo ' ]</p>'."\n";
+    echo '<div class="row"><div class="col-xs-12">';
+	display_saved_queries();
+    echo '</div></div>';
 }
-
 
 // End the transaction
 $db->end_transaction();
@@ -38,6 +26,21 @@ $db->end_transaction();
 			</div>
 		</div>
         <footer class="container">
+<?php
+// Display debug info (if enabled/defined)
+if (defined('LUNA_DEBUG')) {
+	// Calculate script generation time
+	$time_diff = sprintf('%.3f', get_microtime() - $luna_start);
+	echo sprintf(__('Generated in %1$s seconds &middot; %2$s queries executed', 'luna'), $time_diff, $db->get_num_queries());
+
+	if (function_exists('memory_get_usage')) {
+		echo ' &middot; '.sprintf(__('Memory usage: %1$s', 'luna'), file_size(memory_get_usage()));
+
+		if (function_exists('memory_get_peak_usage'))
+			echo ' '.sprintf(__('(Peak: %1$s)', 'luna'), file_size(memory_get_peak_usage()));
+	}
+}
+?>
             <span class="pull-right"><?php printf(__('Powered by %s', 'luna'), ' <a href="http://getluna.org/">Luna '.$luna_config['o_cur_version'].'</a>') ?> '<i><?php echo Version::LUNA_CODE_NAME ?></i>' &middot; <?php echo Version::LUNA_CORE_VERSION ?></span>
         </footer>
 		<script src="../vendor/js/jquery.min.js"></script>
