0ac746456c12fbaa6fed37772593099dfae2d996
Yannick <contact@studio384.be>
2016-04-05 10:40:05 +0200

    Spelling error

--- include/functions.php
+++ include/functions.php
@@ -1474,7 +1474,7 @@ function error($message, $file = null, $line = null, $db_error = false) {
 	</head>
 	<body>
 		<div class="error">
-			<h2>Luna faild to perform your request</h2>
+			<h2>Luna failed to perform your request</h2>
             <p>Something went wrong while performing the action you requested. You can see more details below. It might be useful to enable debug mode for more info. We would like to ask you to report this error to the <a href="http://forum.getluna.org">Luna developers</a> with detailed info on how this error occurred.</p>
 			<div class="error-details">
 <?php
