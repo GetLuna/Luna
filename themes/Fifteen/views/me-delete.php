cb9e1701a0374f1c5f631f9644ee254eb698cd4d
Yannick <contact@studio384.be>
2016-04-09 10:51:52 +0200

    Clean up deletion interface

--- themes/Fifteen/views/me-delete.php
+++ themes/Fifteen/views/me-delete.php
@@ -6,23 +6,30 @@ if (!defined('FORUM'))
 
 ?>
 
-<form id="confirm_del_user" method="post" action="settings.php?id=<?php echo $id ?>">
-	<fieldset>
-		<div class="panel panel-danger">
-			<div class="panel-heading">
-				<h3 class="panel-title"><?php _e('Please confirm that you want to delete the user', 'luna').' <strong>'.luna_htmlspecialchars($username).'</strong>' ?><span class="pull-right"><input type="submit" class="btn btn-danger" name="delete_user_comply" value="<?php _e('Delete', 'luna') ?>" /></span></h3>
-			</div>
-			<div class="panel-body">
-				<?php _e('Deleted users and/or comments cannot be restored. If you choose not to delete the comments made by this user, the comments can only be deleted manually at a later time.', 'luna') ?>
-				<div class="checkbox">
-					<label>
-						<input type="checkbox" name="delete_comments" value="1" checked />
-						<?php _e('Delete all comments and threads this user has made', 'luna') ?>
-					</label>
+<form class="form-horizontal" id="confirm_del_user" method="post" action="settings.php?id=<?php echo $id ?>">
+	<div class="main container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="title-block title-block-danger">
+					<h2><i class="fa fa-fw fa-user"></i> <?php _e('Confirm deletion', 'luna') ?> &middot; <?php echo luna_htmlspecialchars($username) ?><span class="pull-right"><button type="submit" class="btn btn-default" name="delete_user_comply" accesskey="s"><span class="fa fa-fw fa-trash"></span> <?php _e('Delete', 'luna') ?></button></span></h2>
+				</div>
+				<div class="tab-content tab-content-danger">
+                    <div class="alert alert-warning"><i class="fa fa-fw fa-exclamation"></i> <?php _e('Deleted users and/or comments cannot be restored. If you choose not to delete the comments made by this user, the comments can only be deleted manually at a later time.', 'luna') ?></div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label"><?php _e('User content', 'luna') ?></label>
+                        <div class="col-sm-9">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="delete_comments" value="1" checked />
+                                    <?php _e('Delete all comments and threads this user has made.', 'luna') ?>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
 				</div>
 			</div>
 		</div>
-	</fieldset>
+	</div>
 </form>
 <?php
 
