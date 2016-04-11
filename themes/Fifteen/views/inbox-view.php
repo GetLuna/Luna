ea316cbc56104361b51370e4058695fbdf130993
Yannick <contact@studio384.be>
2016-04-11 14:36:58 +0200

    #1579 Solve inbox errors

--- themes/Fifteen/views/inbox-view.php
+++ themes/Fifteen/views/inbox-view.php
@@ -35,7 +35,7 @@ draw_response_list();
 echo $paging_links;
 ?>
 			<form method="post" id="comment" action="new_inbox.php?reply=<?php echo $tid ?>" onsubmit="return process_form(this)">
-                <input type="hidden" name="req_subject" placeholder="<?php _e('Subject', 'luna') ?>" value="<?php echo ($p_subject != '' ? luna_htmlspecialchars($p_subject) : ''); ?>" tabindex="<?php echo $cur_index++ ?>" />
+                <input type="hidden" name="req_subject" placeholder="<?php _e('Subject', 'luna') ?>" value="<?php echo ($p_subject != '' ? luna_htmlspecialchars($p_subject) : ''); ?>" />
                 <input type="hidden" name="form_user" value="<?php echo luna_htmlspecialchars($luna_user['username']) ?>" />
 				<input type="hidden" name="reply" value="1" />
                 <?php draw_editor('10'); ?>
