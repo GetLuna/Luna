20e4bec2688a21e427f166afc073280f85114ec7
Yannick <contact@studio384.be>
2016-04-12 18:08:12 +0200

    #1588 Fix missing ;

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ if ($action == 'disable_cookiebar') {
 if ($luna_config['o_board_slogan'] == '')
     $page_title = array(luna_htmlspecialchars($luna_config['o_board_title']));
 else
-    $page_title = array(luna_htmlspecialchars($luna_config['o_board_title']).' &middot '.$luna_config['o_board_slogan']);
+    $page_title = array(luna_htmlspecialchars($luna_config['o_board_title']).' &middot; '.$luna_config['o_board_slogan']);
 
 define('LUNA_ALLOW_INDEX', 1);
 define('LUNA_ACTIVE_PAGE', 'index');
