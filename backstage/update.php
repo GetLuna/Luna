c5e2185b84eacf230a5c2bad4aa915a17f68618b
Yannick <contact@studio384.be>
2016-05-06 12:11:49 +0200

    Update Font Awesome to version 4.6.2

--- backstage/update.php
+++ backstage/update.php
@@ -188,7 +188,7 @@ if (isset($_GET['saved']))
                     </tr>
                     <tr>
                         <td><?php _e('Font Awesome version', 'luna') ?></td>
-                        <td>4.6.1</td>
+                        <td>4.6.2</td>
                     </tr>
                     <tr>
                         <td><?php _e('jQuery version', 'luna') ?></td>
