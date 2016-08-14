8866527db94d6ffa2e4cbf3e27c0f30eae135191
Yannick <contact@studio384.be>
2016-08-14 16:44:41 +0200

    Drop Sunrise

--- themes/Sunrise/objects/category.php
+++
@@ -1,3 +0,0 @@
-<div class="title-block title-block-primary">
-	<h3><?php echo luna_htmlspecialchars($cur_forum['cat_name']) ?></h3>
-</div>
--- themes/Sunrise/objects/thread2.php
+++
@@ -1,7 +0,0 @@
-<div class="list-group-item <?php echo $item_status ?><?php if ($cur_thread['soft'] == true) echo ' soft'; ?>">
-	<span class="hidden-xs hidden-sm hidden-md hidden-lg">
-		<?php echo forum_number_format($thread_count + $start_from) ?>
-	</span>
-	<a href="<?php echo $url ?>"><?php echo $subject ?></a><span class="hidden-xs"><br /><?php _e('In', 'luna' ) ?></span><span class="hidden-md hidden-sm hidden-lg"> &middot; </span>
-    <?php echo $forum_name ?> &middot; <span class="label label-default"><?php echo forum_number_format($cur_thread['num_replies']) ?></span>
-</div>
