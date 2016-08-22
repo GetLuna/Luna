4c9aafdcb1772179b474113e6140f3609eb97c3b
Yannick <contact@studio384.be>
2016-08-22 13:09:37 +0200

    Clean up language files, version bump

--- include/version.php
+++ include/version.php
@@ -6,23 +6,23 @@
  */
 
 class Version {
-	const LUNA_VERSION = '2.0.3.1';
-    const LUNA_BRANCH = '2.0';
-	const LUNA_CORE_VERSION = '2.0.5754';
-	const LUNA_CODE_NAME = 'Emerald';
+	const LUNA_VERSION = '3.0-alpha.0.1-dev';
+    const LUNA_BRANCH = '3.0';
+	const LUNA_CORE_VERSION = '3.0.6031';
+	const LUNA_CODE_NAME = 'Glitter';
 
 	// The database version number, every change in the database requires this number to go one up
 	const LUNA_DB_VERSION = '91.26';
 
 	// The parser version number, every change to the parser requires this number to go one up
-	const LUNA_PARSER_VERSION = '11.4.1';
+	const LUNA_PARSER_VERSION = '12.0.0';
 
 	// The search index version number, every change to the search index requires this number to go one up
 	const LUNA_SI_VERSION = '2.0';
 
 	// Luna system requirements
 	const MIN_PHP_VERSION = '5.3.0';
-	const MIN_MYSQL_VERSION = '5.0.0';
-	const MIN_PGSQL_VERSION = '8.0.0';
+	const MIN_MYSQL_VERSION = '5.5.0';
+	const MIN_PGSQL_VERSION = '9.1.0';
 }
 ?>
