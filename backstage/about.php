301315620320e10ee274af97497679cfb397400d
Yannick <yannick.franssen@outlook.com>
2018-06-12 20:55:24 +0200

    More Backstage design refinements

--- backstage/about.php
+++ backstage/about.php
@@ -32,8 +32,10 @@ require 'header.php';
 					<div class="container">
 						<h2 class="clearfix"><span class="version-name">Fallow Preview 2 <small>2.1-alpha.2</small></span></h2>
 						<ul class="changes">
+                            <li><div class="stater"><em class="state state-new"><?php _e('New', 'luna') ?></em></div><?php _e('You can now pick a Font Awesome style for forum icons', 'luna') ?></li>
                             <li><div class="stater"><em class="state state-improved"><?php _e('Improved', 'luna') ?></em></div><?php _e('Spoilers now use the Bootstrap\'s collapse plugin', 'luna') ?></li>
                             <li><div class="stater"><em class="state state-improved"><?php _e('Improved', 'luna') ?></em></div><?php _e('Improved database management', 'luna') ?></li>
+                            <li><div class="stater"><em class="state state-improved"><?php _e('Improved', 'luna') ?></em></div><?php _e('Further refinements to the new Backstage design', 'luna') ?></li>
                             <li><div class="stater"><em class="state state-improved"><?php _e('Improved', 'luna') ?></em></div><?php _e('Improved night mode with better contrast and darker design', 'luna') ?></li>
                             <li><div class="stater"><em class="state state-fixed"><?php _e('Fixed', 'luna') ?></em></div><?php _e('ALPHA - Backstage accent settings no longer trigger a warning', 'luna') ?></li>
 						</ul>
