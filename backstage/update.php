7d62ed49d0dae57b08b0d689e99f5a775b7aa46e
Yannick <yannick.franssen@outlook.com>
2018-06-14 00:12:45 +0200

    Merge branch 'version2.0' into version2.1a

--- backstage/update.php
+++ backstage/update.php
@@ -178,7 +178,7 @@ if (version_compare(Version::LUNA_CORE_VERSION, $update_cache, 'lt')) {
 				<h3><?php _e('A new version is available!', 'luna') ?></h3>
 				<p><?php printf(__('A new version, Luna %s has been released. It\'s a good idea to update to the latest version of Luna, as it contains not only new features, improvements and bugfixes, but also the latest security updates.', 'luna'), $update_cache) ?></p>
 				<div class="btn-group">
-					<a href="https://github.com/GetLuna/Luna/archive/preview.zip" class="btn btn-primary"><i class="fa fa-fw fa-download"></i> <?php echo sprintf(__('Download v%s', 'luna'), $update_cache) ?></a>
+					<a href="https://getluna.org" class="btn btn-primary"><i class="fa fa-fw fa-download"></i> <?php echo sprintf(__('Download v%s', 'luna'), $update_cache) ?></a>
 					<a href="http://getluna.org/release" class="btn btn-primary"><i class="fa fa-fw fa-refresh"></i> <?php _e('Changelog', 'luna') ?></a>
 				</div>
 <?php
@@ -189,7 +189,7 @@ if (version_compare(Version::LUNA_CORE_VERSION, $update_cache, 'lt')) {
 <?php
 } else {
 ?>
-				<h3><?php _e('You\'re using a development version of Luna. Be sure to stay up-to-date.', 'luna') ?></h3>
+				<h3><?php _e('You\'re using a preview version of Luna. Be sure to stay up-to-date.', 'luna') ?></h3>
 				<p><?php _e('We release every now and then a new build for Luna, one more stable then the other, for you to check out. You can keep track of this at <a href="http://getluna.org/releases">our website</a>. New builds can contain new features, improved features, and/or bugfixes.', 'luna') ?></p>
 				<p><?php _e('At this point, we can only tell you that you\'re beyond the latest release. We can\'t tell you if there is a new preview available. You\'ll have to find out for yourself.', 'luna') ?></p>
 <?php
--- include/version.php
+++ include/version.php
@@ -9,7 +9,7 @@ class Version
 {
     const LUNA_VERSION = '2.1-alpha.2';
     const LUNA_BRANCH = '2.1';
-    const LUNA_CORE_VERSION = '2.1.5881';
+    const LUNA_CORE_VERSION = '2.1.5898';
     const LUNA_CODE_NAME = 'Fallow';
 
     // The database version number, every change in the database requires this number to go one up
