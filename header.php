57aad6d7c0bdf96112644578b14df5daaf91e04f
Yannick <yannick.franssen@outlook.com>
2018-06-23 10:48:22 +0200

    #1689 #1641 Revamp Fifteen menu, add user icon

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ if (!defined('FORUM')) {
 
 // Send no-cache headers
 header('Expires: Thu, 21 Jul 1977 07:30:00 GMT'); // When yours truly first set eyes on this world! :)
-header('Last-Modified: ' . date('D, d M Y H:i:s') . ' GMT');
+header('Last-Modified: '.date('D, d M Y H:i:s').' GMT');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache'); // For HTTP/1.0 compatibility
 
@@ -23,7 +23,7 @@ header('Content-type: text/html; charset=utf-8');
 
 // Prevent site from being embedded in a frame
 $frame_options = defined('LUNA_FRAME_OPTIONS') ? LUNA_FRAME_OPTIONS : 'deny';
-header('X-Frame-Options: ' . $frame_options);
+header('X-Frame-Options: '.$frame_options);
 
 // Define $p if it's not set to avoid a PHP notice
 $p = isset($p) ? $p : null;
@@ -36,61 +36,56 @@ $page_statusinfo = $page_threadsearches = array();
 
 if (!$luna_user['is_guest']) {
     if (!empty($forum_actions)) {
-        $page_statusinfo[] = '<li>' . implode(' &middot; ', $forum_actions) . '</li>';
+        $page_statusinfo[] = '<li>'.implode(' &middot; ', $forum_actions).'</li>';
     }
 
     if (!empty($thread_actions)) {
-        $page_statusinfo[] = '<li>' . implode(' &middot; ', $thread_actions) . '</li>';
+        $page_statusinfo[] = '<li>'.implode(' &middot; ', $thread_actions).'</li>';
     }
 
     if ($luna_user['is_admmod']) {
         if ($luna_config['o_report_method'] == '0' || $luna_config['o_report_method'] == '2') {
-            $result_header = $db->query('SELECT 1 FROM ' . $db->prefix . 'reports WHERE zapped IS NULL') or error('Unable to fetch reports info', __FILE__, __LINE__, $db->error());
+            $result_header = $db->query('SELECT 1 FROM '.$db->prefix.'reports WHERE zapped IS NULL') or error('Unable to fetch reports info', __FILE__, __LINE__, $db->error());
 
             if ($db->result($result_header)) {
-                $page_statusinfo[] = '<li class="reportlink"><strong><a href="backstage/reports.php">' . __('New reports', 'luna') . '</a></strong></li>';
+                $page_statusinfo[] = '<li class="reportlink"><strong><a href="backstage/reports.php">'.__('New reports', 'luna').'</a></strong></li>';
             }
-
         }
 
         if ($luna_config['o_maintenance'] == '1') {
-            $page_statusinfo[] = '<li class="maintenancelink"><strong><a href="backstage/settings.php#maintenance">' . __('Maintenance mode is enabled', 'luna') . '</a></strong></li>';
+            $page_statusinfo[] = '<li class="maintenancelink"><strong><a href="backstage/settings.php#maintenance">'.__('Maintenance mode is enabled', 'luna').'</a></strong></li>';
         }
-
     }
 
     if ($luna_user['g_read_board'] == '1' && $luna_user['g_search'] == '1') {
-        $page_threadsearches[] = '<a class="list-group-item" href="search.php?action=show_new" title="' . __('Show all new comments since your last visit', 'luna') . '"><span class="fas fa-fw fa-newspaper"></span> ' . __('New', 'luna') . '</a>';
+        $page_threadsearches[] = '<a class="list-group-item" href="search.php?action=show_new" title="'.__('Show all new comments since your last visit', 'luna').'"><span class="fas fa-fw fa-newspaper"></span> '.__('New', 'luna').'</a>';
     }
 
-    $page_threadsearches_inline[] = '<a href="search.php?action=show_new" title="' . __('Show all new comments since your last visit', 'luna') . '"><span class="fas fa-fw fa-newspaper"></span> ' . __('New', 'luna') . '</a>';
+    $page_threadsearches_inline[] = '<a href="search.php?action=show_new" title="'.__('Show all new comments since your last visit', 'luna').'"><span class="fas fa-fw fa-newspaper"></span> '.__('New', 'luna').'</a>';
 }
 
 // Quick searches
 if ($luna_user['g_read_board'] == '1' && $luna_user['g_search'] == '1') {
-    $page_threadsearches[] = '<a class="list-group-item" href="search.php?action=show_recent" title="' . __('Show all active threads', 'luna') . '"><span class="fas fa-fw fa-clock"></span> ' . __('Active', 'luna') . '</a>';
-    $page_threadsearches[] = '<a class="list-group-item" href="search.php?action=show_unanswered" title="' . __('Show all unanswered threads', 'luna') . '"><span class="fas fa-fw fa-question"></span> ' . __('Unanswered', 'luna') . '</a>';
-    $page_threadsearches_inline[] = '<a href="search.php?action=show_recent" title="' . __('Show all active threads', 'luna') . '"><span class="fas fa-fw fa-clock"></span> ' . __('Active', 'luna') . '</a>';
-    $page_threadsearches_inline[] = '<a href="search.php?action=show_unanswered" title="' . __('Show all unanswered threads', 'luna') . '"><span class="fas fa-fw fa-question"></span> ' . __('Unanswered', 'luna') . '</a>';
+    $page_threadsearches[] = '<a class="list-group-item" href="search.php?action=show_recent" title="'.__('Show all active threads', 'luna').'"><span class="fas fa-fw fa-clock"></span> '.__('Active', 'luna').'</a>';
+    $page_threadsearches[] = '<a class="list-group-item" href="search.php?action=show_unanswered" title="'.__('Show all unanswered threads', 'luna').'"><span class="fas fa-fw fa-question"></span> '.__('Unanswered', 'luna').'</a>';
+    $page_threadsearches_inline[] = '<a href="search.php?action=show_recent" title="'.__('Show all active threads', 'luna').'"><span class="fas fa-fw fa-clock"></span> '.__('Active', 'luna').'</a>';
+    $page_threadsearches_inline[] = '<a href="search.php?action=show_unanswered" title="'.__('Show all unanswered threads', 'luna').'"><span class="fas fa-fw fa-question"></span> '.__('Unanswered', 'luna').'</a>';
 }
-
-// Generate all that jazz
-$tpl_temp = '<div id="brdwelcome">';
 
 // The status information
 if (is_array($page_statusinfo)) {
-    $tpl_temp .= "\n\t\t\t" . '<ul class="conl">';
-    $tpl_temp .= "\n\t\t\t\t" . implode("\n\t\t\t\t", $page_statusinfo);
-    $tpl_temp .= "\n\t\t\t" . '</ul>';
+    $tpl_temp .= '<ul class="conl">';
+    $tpl_temp .= implode('', $page_statusinfo);
+    $tpl_temp .= '</ul>';
 } else {
-    $tpl_temp .= "\n\t\t\t" . $page_statusinfo;
+    $tpl_temp .= $page_statusinfo;
 }
 
 // Generate quicklinks
 if (!empty($page_threadsearches)) {
-    $tpl_temp .= "\n\t\t\t" . '<ul class="conr">';
-    $tpl_temp .= "\n\t\t\t\t" . '<li>' . implode(' &middot; ', $page_threadsearches_inline) . '</li>';
-    $tpl_temp .= "\n\t\t\t" . '</ul>';
+    $tpl_temp .= '<ul class="conr">';
+    $tpl_temp .= '<li>'.implode(' &middot; ', $page_threadsearches_inline).'</li>';
+    $tpl_temp .= '</ul>';
 }
 
 $tpl_temp .= '</div>';
@@ -104,58 +99,62 @@ $inbox_menu_item = '';
 $num_new_pm = 0;
 if ($luna_config['o_enable_inbox'] == '1' && $luna_user['g_inbox'] == '1' && $luna_user['use_inbox'] == '1') {
     // Check for new messages
-    $result = $db->query('SELECT COUNT(id) FROM ' . $db->prefix . 'messages WHERE showed=0 AND show_message=1 AND owner=' . $luna_user['id']) or error('Unable to check the availibility of new messages', __FILE__, __LINE__, $db->error());
+    $result = $db->query('SELECT COUNT(id) FROM '.$db->prefix.'messages WHERE showed=0 AND show_message=1 AND owner='.$luna_user['id']) or error('Unable to check the availibility of new messages', __FILE__, __LINE__, $db->error());
     $num_new_pm = $db->result($result);
 
     if ($num_new_pm > 0) {
-        $new_inbox = $num_new_pm . ' ';
+        $new_inbox = $num_new_pm.' ';
     } else {
         $new_inbox = '';
     }
 
-    $inbox_menu_item = '<li><a href="inbox.php"><span class="' . (($num_new_pm > 0) ? ' flash' : '') . '">' . $new_inbox . '<span class="fas fa-fw fa-paper-plane"></span><span class="visible-xs-inline"> ' . __('Inbox', 'luna') . '</span></span></a></li>';
+    $inbox_menu_item = '<li class="nav-item"><a class="nav-link" href="inbox.php"><span'.(($num_new_pm > 0) ? ' class="flash"' : '').'>'.$new_inbox.'<i class="fas fa-fw fa-paper-plane"></i><span class="d-inline d-md-none"> '.__('Inbox', 'luna').'</span></span></a></li>';
 }
 
 if (!$luna_user['is_guest']) {
     // Check for new notifications
-    $result = $db->query('SELECT COUNT(id) FROM ' . $db->prefix . 'notifications WHERE viewed = 0 AND user_id = ' . $luna_user['id']) or error('Unable to load notifications', __FILE__, __LINE__, $db->error());
+    $result = $db->query('SELECT COUNT(id) FROM '.$db->prefix.'notifications WHERE viewed = 0 AND user_id = '.$luna_user['id']) or error('Unable to load notifications', __FILE__, __LINE__, $db->error());
     $num_notifications = $db->result($result);
 
     if ($luna_config['o_notification_flyout'] == 1) {
         if ($num_notifications == '0') {
             $notificon = '<span class="far fa-fw fa-circle"></span>';
-            $ind_notification[] = '<li><a href="notifications.php">' . __('No new notifications', 'luna') . '</a></li>';
+            $ind_notification[] = '<a class="dropdown-item" href="../notifications.php">'.__('No new notifications', 'luna').'</a>';
         } else {
-            $notificon = $num_notifications . ' <span class="fas fa-fw fa-circle"></span>';
+            $notificon = $num_notifications.' <span class="fas fa-fw fa-circle"></span>';
 
-            $notification_result = $db->query('SELECT * FROM ' . $db->prefix . 'notifications WHERE user_id = ' . $luna_user['id'] . ' AND viewed = 0 ORDER BY time DESC LIMIT 10') or error('Unable to load notifications', __FILE__, __LINE__, $db->error());
+            $notification_result = $db->query('SELECT * FROM '.$db->prefix.'notifications WHERE user_id = '.$luna_user['id'].' AND viewed = 0 ORDER BY time DESC LIMIT 10') or error('Unable to load notifications', __FILE__, __LINE__, $db->error());
             while ($cur_notifi = $db->fetch_assoc($notification_result)) {
                 $notifitime = format_time($cur_notifi['time'], false, null, $luna_config['o_time_format'], true, true);
-                $ind_notification[] = '<li class="overflow"><a href="notifications.php?notification=' . $cur_notifi['id'] . '"><span class="timestamp">' . $notifitime . '</span> <span class="fas fa-fw ' . $cur_notifi['icon'] . '"></span> ' . $cur_notifi['message'] . '</a></li>';
+                $ind_notification[] = '<a class="dropdown-item" href="../notifications.php?notification='.$cur_notifi['id'].'"><span class="timestamp">'.$notifitime.'</span> <span class="fas fa-fw '.$cur_notifi['icon'].'"></span> '.$cur_notifi['message'].'</a>';
             }
         }
 
-        $notifications = implode('<li class="divider"></li>', $ind_notification);
+        $notifications = implode('', $ind_notification);
         $notification_menu_item = '
-                        <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="' . (($num_notifications != 0) ? ' flash' : '') . '">' . $notificon . '<span class="visible-xs-inline"> ' . __('Notifications', 'luna') . '</span></span></a>
-                        <ul class="dropdown-menu notification-menu">
-                            <li role="presentation" class="dropdown-header">' . __('Notifications', 'luna') . '</li>
-                            <li class="divider"></li>
-                            ' . $notifications . '
-                            <li class="divider"></li>
-                            <li class="dropdown-footer hidden-xs"><a class="float-right" href="notifications.php">' . __('More', 'luna') . ' <i class="fas fa-fw fa-arrow-right"></i></a></li>
-                            <li class="dropdown-footer hidden-lg hidden-md hidden-sm"><a href="notifications.php">' . __('More', 'luna') . ' <i class="fas fa-fw fa-arrow-right"></i></a></li>
-                        </ul>
-                    </li>';
+            <li class="nav-item dropdown dropdown-notifications">
+                <a class="nav-link dropdown-toggle" id="notificationMenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="'.(($num_notifications != 0) ? 'flash' : '').'">'.$notificon.'</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="notificationMenu">
+                    <h6 class="dropdown-header">'.__('Notifications', 'luna').'</h6>
+                    <div class="dropdown-divider"></div>
+                    '.$notifications.'
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item float-right" href="../notifications.php">'.__('More', 'luna').' <i class="fas fa-fw fa-arrow-right"></i></a>
+                </div>
+            </li>';
     } else {
         if ($num_notifications == '0') {
             $notificon = '<span class="far fa-fw fa-circle"></span>';
         } else {
-            $notificon = $num_notifications . ' <span class="fas fa-fw fa-circle"></span>';
+            $notificon = $num_notifications.' <span class="fas fa-fw fa-circle"></span>';
         }
-
-        $notification_menu_item = '<li><a href="notifications.php" class="' . (($num_notifications != 0) ? ' flash' : '') . '">' . $notificon . '<span class="visible-xs-inline"> ' . __('Notifications', 'luna') . '</span></a></li>';
+    
+        $notification_menu_item ='
+            <li class="nav-item active">
+                <a class="nav-link'.(($num_notifications != 0) ? ' flash' : '').'" href="https://getluna.org/docs" class="'.$notificon.'"><span class="visible-xs-inline"> '.__('Notifications', 'luna').'</span></a>
+            </li>';
     }
 }
 
@@ -163,34 +162,34 @@ if (!$luna_user['is_guest']) {
 if (!$luna_user['is_admmod']) {
     $backstage = '';
 } else {
-    $backstage = '<li><a href="backstage/"><span class="fas fa-fw fa-tachometer-alt"></span><span class="visible-xs-inline"> ' . __('Backstage', 'luna') . '</span></a></li>';
+    $backstage = '<li class="nav-item"><a class="nav-link" href="backstage/"><span class="fas fa-fw fa-tachometer-alt"></span><span class="d-inline d-md-none"> '.__('Backstage', 'luna').'</span></a></li>';
 }
 
-$result = $db->query('SELECT id, url, name, disp_position, visible FROM ' . $db->prefix . 'menu ORDER BY disp_position') or error('Unable to fetch menu items', __FILE__, __LINE__, $db->error());
+$result = $db->query('SELECT id, url, name, disp_position, visible FROM '.$db->prefix.'menu ORDER BY disp_position') or error('Unable to fetch menu items', __FILE__, __LINE__, $db->error());
 
 if ($luna_user['is_guest']) {
-    $usermenu = '<li id="navregister"' . ((LUNA_ACTIVE_PAGE == 'register') ? ' class="active"' : '') . '><a href="register.php">' . __('Register', 'luna') . '</a></li>
-				 <li><a href="#" data-toggle="modal" data-target="#login-form">' . __('Login', 'luna') . '</a></li>';
+    $usermenu = '<li class="nav-item"><a class="nav-link" href="register.php">'.__('Register', 'luna').'</a></li>
+				 <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#login-form">'.__('Login', 'luna').'</a></li>';
 } else {
-    $usermenu = $backstage . $inbox_menu_item . $notification_menu_item . '
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle avatar-item" data-toggle="dropdown"><i class="fas fa-fw fa-user"></i><span class="visible-xs-inline"> ' . luna_htmlspecialchars($luna_user['username']) . '</span> <i class="fas fa-fw fa-angle-down"></i></a>
-					<ul class="dropdown-menu">
-						<li><a href="profile.php?id=' . $luna_user['id'] . '"><i class="fas fa-fw fa-user"></i> ' . __('Profile', 'luna') . '</a></li>
-						<li><a href="settings.php"><i class="fas fa-fw fa-cogs"></i> ' . __('Settings', 'luna') . '</a></li>
-						<li class="divider"></li>
-						<li><a href="help.php"><i class="fas fa-fw fa-info-circle"></i> ' . __('Help', 'luna') . '</a></li>
-						<li class="divider"></li>
-						<li><a href="login.php?action=out&amp;id=' . $luna_user['id'] . '&amp;csrf_token=' . luna_csrf_token() . '"><i class="fas fa-fw fa-sign-out-alt"></i> ' . __('Logout', 'luna') . '</a></li>
-					</ul>
-				</li>
+    $usermenu = $backstage.$inbox_menu_item.$notification_menu_item.'
+        <li class="nav-item dropdown dropdown-user">
+            <a class="nav-link dropdown-toggle" href="profile.php?id='.$luna_user['id'].'" id="profileMenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                '.draw_user_avatar($luna_user['id'], true, 'avatar').'
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="profileMenu">
+                <a class="dropdown-item" href="profile.php?id='.$luna_user['id'].'"><i class="fas fa-fw fa-user"></i>'.__('Profile', 'luna').'</a>
+                <a class="dropdown-item" href="inbox.php"><i class="fas fa-fw fa-paper-plane"></i>'.__('Inbox', 'luna').'</a>
+                <a class="dropdown-item" href="settings.php?id='.$luna_user['id'].'"><i class="fas fa-fw fa-cogs"></i>'.__('Settings', 'luna').'</a>
+                <a class="dropdown-item" href="login.php?action=out&amp;id='.$luna_user['id'].'&amp;csrf_token='.luna_csrf_token().'"><i class="fas fa-fw fa-sign-out-alt"></i>'.__('Logout', 'luna').'</a>
+            </div>
+        </li>
 	';
 }
 
 if ($db->num_rows($result) > 0) {
     while ($cur_item = $db->fetch_assoc($result)) {
         if ($cur_item['visible'] == '1') {
-            $links[] = '<li><a href="' . $cur_item['url'] . '">' . $cur_item['name'] . '</a></li>';
+            $links[] = '<li class="nav-item"><a class="nav-link" href="'.$cur_item['url'].'">'.$cur_item['name'].'</a></li>';
         }
     }
 }
--- themes/Fifteen/views/header.php
+++ themes/Fifteen/views/header.php
@@ -2,7 +2,7 @@
 <html class="<?php echo get_theme_mode() ?>">
 	<head>
 		<?php load_meta(); ?>
-		<link rel="stylesheet" type="text/css" href="vendor/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="vendor/css/bootstrap4.min.css">
         <?php if ($luna_config['o_fontawesomepro'] == 0) { ?>
 		    <link rel="stylesheet" href="vendor/css/fontawesome-all.min.css">
         <?php } else { ?>
@@ -11,39 +11,39 @@
 		<link rel="stylesheet" type="text/css" href="vendor/css/prism.css">
 		<?php load_css(); ?>
 		<script src="vendor/js/jquery.min.js"></script>
-		<script src="vendor/js/bootstrap.min.js"></script>
+		<script src="vendor/js/bootstrap4.min.js"></script>
 		<script src="vendor/js/prism.js"></script>
-		<style>
-        <?php if ($luna_config['o_use_custom_css']) {
-            echo $luna_config['o_custom_css'];
-        } ?>
-        <?php if (($luna_config['o_cookie_bar'] == 1) && ($luna_user['is_guest']) && (!isset($_COOKIE['LunaCookieBar']))) { ?>
-			body { margin-bottom: 60px; }
-			@media screen and (max-width: 767px) { body { margin-bottom: 80px; } }
-        <?php } ?>
+        <?php
+        if ($luna_config['o_use_custom_css']) {
+            echo '<style>'.$luna_config['o_custom_css'].'</style>';
+        }
+        if (($luna_config['o_cookie_bar'] == 1) && ($luna_user['is_guest']) && (!isset($_COOKIE['LunaCookieBar']))) {
+            echo '<style>';
+			echo 'body { margin-bottom: 60px; }';
+			echo '@media screen and (max-width: 767px) { body { margin-bottom: 80px; } }';
+            echo '</style>';
+        }
+        ?>
 		</style>
 	</head>
 	<body>
 		<?php if ($luna_user['is_guest']): require load_page('login.php'); endif; ?>
-        <div id="header">
-            <div class="navbar navbar-inverse navbar-static-top">
+        <div id="header">   
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-primary-collapse">
-                            <span class="icon-bar<?php echo (($num_new_pm != 0 || $num_notifications != 0)? ' flash' : '') ?>"></span>
-                            <span class="icon-bar<?php echo (($num_new_pm != 0 || $num_notifications != 0)? ' flash' : '') ?>"></span>
-                            <span class="icon-bar<?php echo (($num_new_pm != 0 || $num_notifications != 0)? ' flash' : '') ?>"></span>
-                        </button>
-                        <a class="navbar-brand" href="index.php"><?php echo $menu_title ?></a>
-                    </div>
-                    <div class="navbar-primary-collapse navbar-collapse collapse">
-                        <ul class="nav navbar-nav"><?php echo implode("\n\t\t\t\t", $links); ?></ul>
-                        <ul class="nav navbar-nav navbar-right"><?php echo $usermenu; ?></ul>
+                    <a class="navbar-brand" href="index.php"><?php echo $menu_title ?></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon<?php echo (($num_new_pm != 0 || $num_notifications != 0)? ' flash' : '') ?>"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <?php echo implode('', $links); ?>
+                        </ul>
+                        <ul class="navbar-nav my-2 my-lg-0">
+                            <?php echo $usermenu; ?>
+                        </ul>
                     </div>
                 </div>
-            </div>
+            </nav>
         </div>
-
-
-
-        
