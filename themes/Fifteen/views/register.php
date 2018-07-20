bf2a48686ec6d8de4171d31ce87f00811ca5ac89
Yannick <yannick.franssen@outlook.com>
2018-07-20 09:46:23 +0200

    #1707 Do'nt return password on error

--- themes/Fifteen/views/register.php
+++ themes/Fifteen/views/register.php
@@ -28,10 +28,10 @@ if (!defined('FORUM'))
 						<div class="col-sm-9">
 							<div class="row">
 								<div class="col-sm-6">
-									<input id="password" type="password" class="form-control" name="req_password1" value="<?php if (isset($_POST['req_password1'])) echo luna_htmlspecialchars($_POST['req_password1']); ?>" />
+									<input id="password" type="password" class="form-control" name="req_password1" />
 								</div>
 								<div class="col-sm-6">
-									<input type="password" class="form-control" name="req_password2" value="<?php if (isset($_POST['req_password2'])) echo luna_htmlspecialchars($_POST['req_password2']); ?>" />
+									<input type="password" class="form-control" name="req_password2" />
 								</div>
 							</div>
 						</div>
