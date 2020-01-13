5d546ca1a6efc25d79fbd3f795151b8d17d1d03f
Yannick <yannick.franssen@outlook.com>
2020-01-13 19:34:48 +0100

    Luna 2.0.13

--- include/version.php
+++ include/version.php
@@ -7,8 +7,8 @@
 
 class Version {
 	const LUNA_VERSION = '2.0.13';
-    const LUNA_BRANCH = '2.0';
-	const LUNA_CORE_VERSION = '2.0.5834';
+	const LUNA_BRANCH = '2.0';
+	const LUNA_CORE_VERSION = '2.0.5835';
 	const LUNA_CODE_NAME = 'Emerald';
 
 	// The database version number, every change in the database requires this number to go one up
